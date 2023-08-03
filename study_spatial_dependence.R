71fce928903833a3c41e80531aeb3d226213fb22
Bratkova <bratkova@lancaster.ac.uk>
2023-08-03 10:36:42 +0100

    Remove extra lines from loading from .nc file

--- study_spatial_dependence.R
+++ study_spatial_dependence.R
@@ -48,33 +48,22 @@ pp.ll.to.rg <- function(lat,long,pole.lat,pole.long) {
   while(long.rotated < -180) long.rotated <- long.rotated+360
   return(c(lat.rotated,long.rotated))
 }
-cr <- '\n'
 
-### read one year of data
+### read one year of data ----
 flist  <- "data/tasmax_rcp85_land-cpm_uk_2.2km_01_day_19991201-20001130.nc"
-
 ### read netcdf file
-cat("loading",basename(flist[1]),cr) 
 nc1 <- nc_open(flist[1])
-# all variables and metadata in file
-vlist <- nc1$var
 # time coordinate
 time1    <- ncvar_get(nc1,'time')
 time1att <- ncatt_get(nc1,'time')
-# rainfall rate
 # read all 360 days
 # time dimension is last in the start argument
 v1    <- ncvar_get(nc1,'tasmax',start=c(1,1,1,1), count=c(-1,-1,360,1))
-# names(vlist) contains list of variables 
-v1att <- ncatt_get(nc1,'tasmax')
 # lat long of grid
 rlon     <- apply(ncvar_get(nc1,"grid_longitude_bnds"),2,mean)
 rlat     <- apply(ncvar_get(nc1,"grid_latitude_bnds"),2,mean)
 cpm.pole <- ncatt_get(nc1,'rotated_latitude_longitude')
 nc_close(nc1)
-cat("Variable:",v1att$standard_name,cr)
-cat("Units:",v1att$units,cr)
-
 # this climate model has a 360 day year so needs special treatment
 # PCICt does this for us
 origin   <- tail(unlist(strsplit(time1att$units,'hours since ')),1)
@@ -83,13 +72,6 @@ pcictime <- as.PCICt( time1*(60*60), cal=time1att$calendar ,origin=origin)
 # the CPM grid has the pole in a different place to usual.  therefore need to transform the map coordinates
 gr_npole_lat <- cpm.pole$grid_north_pole_latitude
 gr_npole_lon <- cpm.pole$grid_north_pole_longitude
-n.map <- maps::map('worldHires', xlim=c(-10,10),ylim=c(30,70),interior=F,plot=FALSE)
-r.map <- n.map
-for (i in 1:length(r.map$x)){
-  if(!is.na(n.map$y[i]) & !is.na(n.map$x[i])) r.latlon   <- pp.ll.to.rg(n.map$y[i],n.map$x[i], gr_npole_lat, gr_npole_lon)
-  r.map$x[i] <- r.latlon[2]
-  r.map$y[i] <- r.latlon[1]
-}
 
 
 ### plot a single field -----
@@ -103,7 +85,6 @@ latitude <- latitude[lat_subset]
 v1_sub <- v1[lon_subset,lat_subset,]
 
 fields::image.plot( x=longitude, y=latitude, z=v1_sub[,,1], zlim=c(0,20), main=pcictime[2])
-points(r.map$x+360,r.map$y,pch=46,col='grey80', lwd=0)
 
 ### subset to show only Lancaster district ----
 #m <- st_read("cnty/infuse_cnty_lyr_2011.shp")
@@ -132,15 +113,6 @@ uk_rot <-  df %>%
   st_as_sf(coords = c("lon", "lat"), crs = 4326) %>%
   summarise(geometry = st_combine(geometry)) %>%
   st_cast("POLYGON") %>% st_make_valid()
-# check by mapping
-tm_shape(uk_rot) + tm_polygons()
-
-# study point rotation
-# df1 <- data.frame(X=long_rot,Y=lat_rot,type=rep("Rotated",length(long_rot)))
-# df2 <- data.frame(X=long,Y=lat,type=rep("Original",length(long)))
-# #df <- rbind(df1,df2)
-# ggplot() + geom_point(data=df1,mapping=aes(x=X,y=Y))+ geom_point(data=df2,mapping=aes(x=X,y=Y)) 
-# can play further to only include UK shapefile
 
 ### subset rotated grid points overlapping with UK -----
 # create grid points for long lat
@@ -149,6 +121,10 @@ colnames(lon_lat) <- c("lon","lat")
 # extract first half-hour of the day (first out of 48 in dim3:time)
 Temperature <- c(v1_sub[,,1])
 lon_lat_temp <- cbind(lon_lat,Temperature)
+for (i in 2:(dim(v1_sub)[3]/12)) {
+  lon_lat_temp <- cbind(lon_lat_temp,c(v1_sub[,,i]))
+  colnames(lon_lat_temp)[length(lon_lat_temp)] <- as.character(i)
+}
 # convert to sf points object
 temp_sf <- st_as_sf(lon_lat_temp,coords = c("lon","lat"),crs=4326)
 tm_shape(temp_sf) + tm_dots(col="Temperature",style="cont",palette="viridis")
