ba45ad23cd256074d4cc331e2f695abf54bb18d5
Kika0 <kbratkov0@gmail.com>
2023-11-30 13:02:17 +0000

    Move functions to a new script

--- conditional_extremes.R
+++ conditional_extremes.R
@@ -1,7 +1,9 @@
+# libraries and functions ----
 library(evd)
 library(tidyverse)
 library(latex2exp)
 library(gridExtra)
+source("cond_model_helpers")
 
 # set theme defaults to be black rectangle
 theme_set(theme_bw())
@@ -12,42 +14,14 @@ theme_replace(
   strip.background = element_blank(),
   panel.border = element_rect(colour = "black", fill = NA) )
 
-generate_dep_X_Y_Y_Z <- function(N,dep=c(1/2,1/2)) {
-  dat <- tibble(x1=numeric(),x2=numeric(),x3=numeric(),R=numeric(),u=numeric(),q=numeric(),w1=numeric(),w2=numeric(),w3=numeric())
- # set.seed(12)
-  x_y <- evd::rbvevd(N,dep=dep[1],model="log")
-  x <- exp(x_y[,1])
-  Y <- exp(x_y[,2])
-  a <- dep[2]
-  # generate z
-  to_opt <- function(z) {
-    (  (  y^(-(1/a)+1)*(y^(-1/a)+z^(-1/a))^(a-1)*exp(-(y^(-1/a)+z^(-1/a))^a)*exp(1/y)  )-Unif)^2
-  }
-  z <- c()
-  for (i in 1:nrow(x_y)){
-    Unif <- runif(1) # generate U
-    y <- Y[i]
-    z[i] <- optim(par=1,fn=to_opt)$par
-  }
-  sims <- data.frame(X_1=x,X_2=Y,X_3=z)
-  return(sims)
-}
 
-# generate trivariate sample
+# generate trivariate sample ----
 N <- 5000
 sims <- generate_dep_X_Y_Y_Z(N=N)
 
 ggplot(sims %>% pivot_longer(everything())) + geom_density(aes(x=value),,stat="density") + facet_wrap(~name)
 
-frechet_laplace_pit <- function(x) {
-  if (exp(-1/x)<0.5) {
-    y <-log(2*exp(-1/x))
-  }
-  else {
-    y <--log(2*(1-exp(-1/x)))
-  }
-  return(y)
-}
+
 
 # PIT to Laplace
 sims <- sims %>% mutate(Y_1=as.numeric(map(.x=X_1,.f=frechet_laplace_pit))) %>% 
@@ -90,17 +64,6 @@ Y_2_sim <- rnorm(n=length(Y_1),mean = a_hat*Y_1 + Y_1^b_hat*mu_hat,sd=sig_hat*Y_
 
 Y_given_1_extreme <- Y_given_1_extreme %>% mutate(Y_2_sim=Y_2_sim)
 ggplot(Y_given_1_extreme %>% select(Y_1,Y_2_sim,Y_2,Y_3) %>% pivot_longer(everything())) + geom_density(aes(x=value),,stat="density") + facet_wrap(~name)
-
-# transform back to Fréchet margins
-laplace_frechet_pit <- function(y) {
-  if (y<0) {
-    x <- (log(2)-y)^(-1)
-  }
-  else {
-    x <- -(log(1-exp(-(y+log(2)))))^(-1)
-  }
-  return(x)
-}
 
 Y_2_sim <- bind_rows(Y_not_1_extreme %>% mutate(Y_2_sim=Y_2),Y_given_1_extreme) %>% 
   mutate(X_2_sim=rep(0,N))
@@ -231,6 +194,7 @@ Gen_Y_1 <- Gen_Y_1 %>% mutate(Y_2=Y_2) %>% mutate(sim=rep("conditional_model",N)
 Gen_orig <- rbind(Gen_Y_1,Y_given_1_extreme %>% select(X_1,Y_1,Y_2) %>% mutate(sim=rep("original_laplace",50)))
 ggplot(Gen_orig) + geom_point(aes(x=Y_1,y=Y_2,col=sim),alpha=0.5) + scale_color_manual(values = c("original_laplace"="black",
                                                                                                  "conditional_model" = "#C11432")) 
+(Gen_Y_1 %>% filter(Y_1>9,Y_1<12,Y_2>9,Y_2<12) %>% dim())[1]/50000
 
-
-
+evd::pbvevd(c(12to frechet,12),dep=dep[1],model="log") -evd::pbvevd(c(9,12),dep=dep[1],model="log") -
+  evd::pbvevd(c(12,9),dep=dep[1],model="log") + evd::pbvevd(c(9,9),dep=dep[1],model="log")
