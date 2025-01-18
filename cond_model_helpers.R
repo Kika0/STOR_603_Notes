5287be27bce495eac79f0725b2e57dfd06b5e9df
Kika0 <kbratkov0@gmail.com>
2025-01-18 21:50:14 +0000

    Change initial values for AGG* methods of par_est

--- cond_model_helpers.R
+++ cond_model_helpers.R
@@ -106,7 +106,6 @@ par_est <- function(df=sims,v=0.99,given=c(1),margin="AGG",method="two_step", a=
         init_parb <- c(b_max/2,0,1)
         #optb <- optim(par=init_parb,fn = Y_likelihood,df=Y_given1extreme,given=j,sim=res[i-1],a_hat=opta$par[1],lower=c(0,-Inf,0),upper = c(b_max,Inf,4),control = list(fnscale=-1,maxit=2000), method = "L-BFGS-B")
         optb <- optim(par=init_parb,fn = Y_likelihood,df=Y_given1extreme,given=j,sim=res[i-1],a_hat=opta$par[1],b_max=b_max,control = list(fnscale=-1,maxit=2000), method = "Nelder-Mead")
-        
         b_hat <- append(b_hat,optb$par[length(optb$par)-2])
         optmusig <- optim(par=init_parb,fn = Y_likelihood,df=Y_given1extreme,given=j,sim=res[i-1],a_hat=opta$par[1],b_hat=optb$par[2],control = list(fnscale=-1,maxit=2000))
         mu_hat <- append(mu_hat,optmusig$par[length(optmusig$par)-1])
@@ -143,7 +142,8 @@ par_est <- function(df=sims,v=0.99,given=c(1),margin="AGG",method="two_step", a=
       }
       
       if (margin=="GenGaus" & method!="one_step") {
-        opt <- optim(fn=NLL_GenGaus,x=(Y2-a_hat[length(a_hat)]*Y1)/(Y1^b_hat[length(b_hat)]),par=c(0,1,1.5),control=list(maxit=2000),method = "SANN")
+        Z2 <- (Y2-a_hat[length(a_hat)]*Y1)/(Y1^b_hat[length(b_hat)])
+        opt <- optim(fn=NLL_GenGaus,x=Z2,par=c(mean(Z2),sd(Z2),1.5),control=list(maxit=2000),method = "SANN")
         mu_agg_hat <- append(mu_agg_hat,opt$par[1])
         sig_agg_hat <- append(sig_agg_hat,opt$par[2])
         delta_hat <- append(delta_hat,opt$par[3])
@@ -151,7 +151,8 @@ par_est <- function(df=sims,v=0.99,given=c(1),margin="AGG",method="two_step", a=
       }
       
       if (margin=="AGG" & method!="one_step") {
-        opt <- optim(fn=NLL_AGG,x=(Y2-a_hat[length(a_hat)]*Y1)/(Y1^b_hat[length(b_hat)]),par=c(0,1,1.2,1.8),control=list(maxit=2000),method = "SANN")
+        Z2 <- (Y2-a_hat[length(a_hat)]*Y1)/(Y1^b_hat[length(b_hat)])
+        opt <- optim(fn=NLL_AGG,x=Z2,par=c(mean(Z2),sd(Z2),1.2,1.8),control=list(maxit=2000),method = "SANN")
         mu_agg_hat <- append(mu_agg_hat,opt$par[1])
         sig_agg_hat <- append(sig_agg_hat,opt$par[2])
         deltal_hat <- append(deltal_hat,opt$par[3])
@@ -160,7 +161,8 @@ par_est <- function(df=sims,v=0.99,given=c(1),margin="AGG",method="two_step", a=
       }
       
       if (margin=="AGGsig" & method!="one_step") {
-        opt <- optim(fn=NLL_AGGsig,x=(Y2-a_hat[length(a_hat)]*Y1)/(Y1^b_hat[length(b_hat)]),par=c(0,1,1,1.5),control=list(maxit=2000),method = "SANN")
+        Z2 <- (Y2-a_hat[length(a_hat)]*Y1)/(Y1^b_hat[length(b_hat)])
+        opt <- optim(fn=NLL_AGGsig,x=Z2,par=c(mean(Z2),sd(Z2),sd(Z2),1.5),control=list(maxit=2000),method = "SANN")
         mu_agg_hat <- append(mu_agg_hat,opt$par[1])
         sigl_hat <- append(sigl_hat,opt$par[2])
         sigu_hat <- append(sigu_hat,opt$par[3])
@@ -168,7 +170,8 @@ par_est <- function(df=sims,v=0.99,given=c(1),margin="AGG",method="two_step", a=
         lik2 <- append(lik2,-opt$value)
       }
       if (margin=="AGGsigdelta" & method!="one_step") {
-        opt <- optim(fn=NLL_AGGsigdelta,x=(Y2-a_hat[length(a_hat)]*Y1)/(Y1^b_hat[length(b_hat)]),par=c(0,1,1,1.2,1.8),control=list(maxit=2000),method = "SANN")
+        Z2 <- (Y2-a_hat[length(a_hat)]*Y1)/(Y1^b_hat[length(b_hat)])
+        opt <- optim(fn=NLL_AGGsigdelta,x=Z2,par=c(mean(Z2),sd(Z2),sd(Z2),1.2,1.8),control=list(maxit=2000),method = "SANN")
         mu_agg_hat <- append(mu_agg_hat,opt$par[1])
         sigl_hat <- append(sigl_hat,opt$par[2])
         sigu_hat <- append(sigu_hat,opt$par[3])
