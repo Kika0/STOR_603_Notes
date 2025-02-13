ca09e1c99d69194fa2abd510eec40ac1f72a1089
Kika0 <kbratkov0@gmail.com>
2025-02-13 17:02:01 +0000

    Try change filepath for UKgrid5km

--- simon_stationary_dataPIT.R
+++ simon_stationary_dataPIT.R
@@ -14,3 +14,61 @@ summary(chosen.MSgpd)
 # load the Gpd parameters
 load("../kristina/MSGpdParam/ukgd_cpm85_5k_x100y23.MSGpdParam.2024-10-22-064747.RData")
 glimpse(gpdpar) # dataframe for columns for scale, shape and threshold
+
+# source("grid-info-5km.R")
+
+library(ncdf4)
+library(fields)
+
+###############################################################################
+# 5km
+#
+# NOTE the obs are on a bigger grid than the CPM grid
+###############################################################################
+obs_cpm_offset_y <- 33  # the difference in size of the y dimension
+
+xcoord_m         <- 146 # London x146 y44 on the CPM grid from Laura's city file.
+ycoord_m         <- 44
+xcoord_o         <- xcoord_m
+ycoord_o         <- ycoord_m + obs_cpm_offset_y
+
+### OBS 5km
+setwd("../../")
+obs_example  <- '../../../../z/kristina/UKgrid5km/tasmax_rcp85_land-cpm_uk_5km_01_ann_206012-208011.nc'
+nc1      <- nc_open(obs_example)
+vlist    <- nc1$var
+shape.o  <- vlist$tasmax$size
+tas5.o   <- ncvar_get(nc1, "tasmax")
+lon5.o   <- ncvar_get(nc1, "longitude")
+lat5.o   <- ncvar_get(nc1, "latitude")
+nc_close(nc1)
+
+
+### CPM 5km
+cpm2k_example <- "/project/ukcp/land-cpm/uk/5km/rcp85/01/tasmax/day/v20210615/tasmax_rcp85_land-cpm_uk_5km_01_day_19901201-20001130.nc"
+nc1      <- nc_open(cpm2k_example)
+vlist    <- nc1$var
+shape.m  <- vlist$tasmax$size
+tas5.m   <- ncvar_get(nc1, "tasmax")
+lon5.m   <- ncvar_get(nc1, "longitude")
+lat5.m   <- ncvar_get(nc1, "latitude")
+nc_close(nc1)
+
+
+par(mfcol=c(2,2))
+
+quilt.plot(as.vector(lon5.o), as.vector(lat5.o), tas5.o[,,10],  nx=shape.o[1], ny=shape.o[2], main='OBS 5km')
+quilt.plot(as.vector(lon5.m), as.vector(lat5.m), tas5.m[,,180], nx=shape.m[1], ny=shape.m[2], main='CPM 5km')
+
+image.plot(tas5.o[,,10],  main='OBS 5km')
+image.plot(tas5.m[,,180], main='CPM 5km')
+
+#  str(tas5.o) num [1:180, 1:290, 1:31] NA NA NA NA NA NA NA NA NA NA ...
+#  str(lon5.o) num [1:180, 1:290] -9.99 -9.92 -9.86 -9.79 -9.72 ...
+#  str(lat5.o) num [1:180, 1:290] 47.8 47.8 47.9 47.9 47.9 ...
+#
+#  str(tas5.m) num [1:180, 1:244, 1:3600] 10.1 10.2 10.3 10.3 10.2 ...
+#  str(lon5.m) num [1:180, 1:244] -10.23 -10.16 -10.09 -10.02 -9.95 ...
+#  str(lat5.m) num [1:180, 1:244] 49.3 49.3 49.3 49.3 49.3 ...
+
+save(file="grid-info-5km.RData", lon5.o, lat5.o, lon5.m, lat5.m, obs_example, cpm2k_example)
