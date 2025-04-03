73495956edea6ed5eb76140aa985285429801947
Kika0 <kbratkov0@gmail.com>
2025-04-03 23:28:32 +0100

    Minor edits

--- spatial_parameter_estimation.R
+++ spatial_parameter_estimation.R
@@ -4,7 +4,7 @@
 #'  residual margin parameters and links back to spatial locations
 #'
 #' @param data_Lap A dataframe of observed data on Laplace scale Y
-#' @param cond_sites A vector of conditioning sites (a subset of 12 possible sites)
+#' @param cond_sites A dataframe of conditioning sites (a subset of df_sites)
 #' @param dayshift A numeric vector of temporal lags (days)
 #' @param v A quantile
 #' @param ab_method A method to estimate dependence parameters $\alpha$ and $\beta$
@@ -15,21 +15,7 @@
 #' @export
 #'
 #' @examples
-spatial_par_est <- function(data_Lap,cond_sites,dayshift=c(0),v=0.9,ab_method="sequential2",res_margin="AGG",grid_uk=xyUK20_sf) {
-  Birmingham <- c(-1.9032,52.4806)
-  Glasgow <- c(-4.258109,55.859112)
-  London <- c(-0.127676,51.529972)
-  Inverness <- c(-4.22498,57.48065) # Inverness bus station
-  Lancaster <- c(-2.78440,54.00871) # PSC building Lancaster University
-  Newcastle <- c(-1.61682,54.96902) # Newcastle railway station
-  Cromer <- c(1.28486,53.05349)
-  Hull <- c(-0.335827,53.767750)
-  Lowestoft <- c(1.72431,52.48435)
-  Truro <- c(-5.05125342465549,50.263075821232704)
-  Dolgellau <- c(-3.8844362867080897,52.74213275545185)
-  Bournemouth <- c(-1.8650607066137428,50.72173094587856)
-  df_sites <- data.frame(Birmingham,Glasgow,London,Inverness,Lancaster,Newcastle,Cromer,Hull,Lowestoft,Truro,Dolgellau,Bournemouth)
-  
+spatial_par_est <- function(data_Lap,cond_sites,dayshift=c(0),Ndays_season=90,v=0.9,ab_method="sequential2",res_margin="AGG",grid_uk=xyUK20_sf) {
   est_all <- data.frame("lik" = numeric(), "lika"= numeric(),"likb"= numeric(),"lik2"= numeric(),
                             "a" = numeric(), "b" = numeric(),
                             "mu" = numeric(),"mu_agg" = numeric(),
@@ -39,13 +25,13 @@ spatial_par_est <- function(data_Lap,cond_sites,dayshift=c(0),v=0.9,ab_method="s
   for (i in 1:ncol(cond_sites)) {
     cond_site <- find_site_index(as.numeric(cond_sites[,i]),grid_uk = grid_uk)
     for (j in 1:length(dayshift)) {
-      sims_tau <- shift_time(sims=data_Lap,cond_site=cond_site,tau=dayshift[j],Ndays_season = 90)
+      sims_tau <- shift_time(sims=data_Lap,cond_site=cond_site,tau=dayshift[j],Ndays_season = Ndays_season)
       pe <- par_est(df=sims_tau,v=v,given=cond_site,margin = "Normal", method=ab_method,keef_constraints = c(1,2))
       # calculate observed residuals
       obsr <- observed_residuals(df=data_Lap,given=cond_site,v = v,a=pe$a,b=pe$b)
       # estimate residual margin parameters
       pe_res <- res_margin_par_est(obs_res = obsr,method="AGG")
-      est_all <- rbind(est_all,cbind(pe_res,pe) %>% add_row(.before=cond_site) %>%  mutate(cond_site=names(df_sites[i]),tau=as.character(dayshift[j])))
+      est_all <- rbind(est_all,cbind(pe_res,pe) %>% add_row(.before=cond_site) %>%  mutate(cond_site=names(cond_sites[i]),tau=as.character(dayshift[j])))
        }
   }
   est_all <- est_all %>% mutate(tau=factor(as.character(tau),levels = as.character(dayshift)))
