d509e7e033c34cad136a6c34c6bd0e2da84d08a0
Kika0 <kbratkov0@gmail.com>
2025-06-04 16:08:55 +0100

    Adapt to allow for varying the threshold for conditioning

--- simon/first40_last40_years_comparison.R
+++ simon/first40_last40_years_comparison.R
@@ -1,6 +1,7 @@
-library(tmap)
+library(tmap) # spatial map plots
+library(sf) # for handling spatial sf objects
 library(tidyverse)
-library(latex2exp)
+library(latex2exp) # latex expressions for plot labels
 theme_set(theme_bw())
 theme_replace(
   panel.spacing = unit(2, "lines"),
@@ -10,7 +11,7 @@ theme_replace(
   panel.border = element_rect(colour = "black", fill = NA) )
 file.sources = list.files(pattern="*helpers.R")
 sapply(file.sources,source,.GlobalEnv)
-# source("spatial_parameter_estimation.R") # for spatial_par_est function
+source("spatial_parameter_estimation.R") # for spatial_par_est function
 load("data_processed/temperature_data.RData")
 load("data_processed/spatial_helper.RData")
 Birmingham <- c(-1.9032,52.4806)
@@ -27,18 +28,19 @@ Dolgellau <- c(-3.8844362867080897,52.74213275545185)
 Bournemouth <- c(-1.8650607066137428,50.72173094587856)
 df_sites <- data.frame(Birmingham,Glasgow,London,Inverness,Lancaster,Newcastle,Cromer,Hull,Lowestoft,Truro,Dolgellau,Bournemouth)
 #spatial_par_est saves parameter estimates as est_all_sf sf object in ../Documents folder
-#spatial_par_est(data_Lap = data_mod_Lap,cond_sites = df_sites,dayshift = 0,Ndays_season = 90,title = "all12sites")
-#spatial_par_est(data_Lap = data_mod_Lap[1:3600,],cond_sites = df_sites,dayshift = 0,Ndays_season = 90,title = "first40_12sites")
-#spatial_par_est(data_Lap = data_mod_Lap[5401:9000,],cond_sites = df_sites,dayshift = 0,Ndays_season = 90,title = "last40_12sites")
+q <- 0.94 # quantile threshold
+spatial_par_est(data_Lap = data_mod_Lap,cond_sites = df_sites,dayshift = 0,v=q,Ndays_season = 90,title = paste0("all12sites",q*100))
+spatial_par_est(data_Lap = data_mod_Lap[1:3600,],cond_sites = df_sites,dayshift = 0,v=q,Ndays_season = 90,title = paste0("first40_12sites",q*100))
+spatial_par_est(data_Lap = data_mod_Lap[5401:9000,],cond_sites = df_sites,dayshift = 0,v=q,Ndays_season = 90,title = paste0("last40_12sites",q*100))
 
 # load all three parameter estimates sf objects ----
-load("data_processed/N9000_sequential2_AGG_all12sites.RData")
+load(paste0("data_processed/N9000_sequential2_AGG_all12sites_q",q*100,".RData"))
 est_all <- est_all_sf
 
-load("data_processed/N3600_sequential2_AGG_first40_12sites.RData")
+load("data_processed/N3600_sequential2_AGG_first40_12sites_q",q*100,".RData")
 est_allfirst40 <- est_all_sf
 
-load("data_processed/N3600_sequential2_AGG_last40_12sites.RData")
+load("data_processed/N3600_sequential2_AGG_last40_12sites_q",q*100,".RData")
 est_alllast40 <- est_all_sf
 
 # combine the first and the last 40 years
@@ -55,8 +57,8 @@ p2 <- ggplot(est_comb) + geom_density(aes(x=b,fill=Year_range),alpha=0.5)  +  sc
 
 
 
-ggsave(p1,width=10,height=5,filename = "../Documents/acomb_12sites.png")
-ggsave(p2,width=10,height=5,filename = "../Documents/bcomb_12sites.png")
+ggsave(p1,width=10,height=5,filename = paste0("../Documents/acomb_12sites_q",q*100,".png"))
+ggsave(p2,width=10,height=5,filename = paste0("../Documents/bcomb_12sitesq",q*100,".png"))
 
 # plot estimates spatially for each site ------
 # bind data together
@@ -64,9 +66,9 @@ est_comball <- bind_rows(est_all %>% mutate("Year_range"=c("1981_2080")),est_all
 est_comball <- est_comball %>% mutate(Year_range=factor(Year_range,levels=c("1981_2080","1981_2020","2041_2080")))
 for (i in 1:12) {
   pa <- tm_shape(est_comball %>% filter(cond_site==names(df_sites)[i]))  + tm_dots(col="a",palette="viridis",n=8,size=0.3,colorNA="aquamarine",title=TeX("$\\alpha$"),textNA = "Conditioning site") + tm_facets(by= c("cond_site","Year_range"))+  tm_layout(legend.outside.size=0.3,asp=0.5,legend.text.size = 1,legend.title.size=1.5)
-    tmap_save(pa,filename=paste0("../Documents/acomp_",names(df_sites[i]),".png"),width=10,height=6)
+    tmap_save(pa,filename=paste0("../Documents/acomp_q",q*100,"_",names(df_sites[i]),".png"),width=10,height=6)
     pb <- tm_shape(est_comball %>% filter(cond_site==names(df_sites)[i]))  + tm_dots(col="b",palette="viridis",n=8,size=0.3,colorNA="aquamarine",title=TeX("$\\beta$"),textNA = "Conditioning site") + tm_facets(by= c("cond_site","Year_range"))+  tm_layout(legend.outside.size=0.3,asp=0.5,legend.text.size = 1,legend.title.size=1.5)
-    tmap_save(pb,filename=paste0("../Documents/bcomp_",names(df_sites[i]),".png"),width=10,height=6)
+    tmap_save(pb,filename=paste0("../Documents/bcomp_q",q*100,"_",names(df_sites[i]),".png"),width=10,height=6)
     
     }
 
