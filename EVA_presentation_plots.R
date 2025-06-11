fb83bfd8daabb2fbc87d940a37a3c26eafdcb6c1
Kika0 <kbratkov0@gmail.com>
2025-06-11 08:58:36 +0100

    Add second plot for illustrating the conditional model

--- EVA_presentation_plots.R
+++ EVA_presentation_plots.R
@@ -1,4 +1,5 @@
 #library(tmap) # spatial map plots
+library(gridExtra)
 library(sf) # for handling spatial sf objects
 library(tidyverse)
 library(latex2exp) # latex expressions for plot labels
@@ -13,7 +14,7 @@ file.sources = list.files(pattern="*helpers.R")
 sapply(file.sources,source,.GlobalEnv)
 source("spatial_parameter_estimation.R") # for spatial_par_est function
 load("data_processed/temperature_data.RData")
-#load("data_processed/spatial_helper.RData")
+load("data_processed/spatial_helper.RData")
 Birmingham <- c(-1.9032,52.4806)
 Glasgow <- c(-4.258109,55.859112)
 London <- c(-0.127676,51.529972)
@@ -29,6 +30,21 @@ Bournemouth <- c(-1.8650607066137428,50.72173094587856)
 df_sites <- data.frame(Birmingham,Glasgow,London,Inverness,Lancaster,Newcastle,Cromer,Hull,Lowestoft,Truro,Dolgellau,Bournemouth)
 
 # 1. plot of Birmingham and Glasgow on original and on Laplace margins
-dim(data_mod)
-summary(data_mod[,1:10])
-find_site_index(site=Birmingham,grid_uk = x)
+Birm_index <- find_site_index(site=Birmingham,grid_uk = xyUK20_sf)
+Glasgow_index <- find_site_index(site=Glasgow,grid_uk = xyUK20_sf)
+
+tmp <- data_mod[,c(Birm_index,Glasgow_index)]
+names(tmp) <- names(df_sites)[1:2]
+p1 <- ggplot(tmp) + geom_point(aes(x=Birmingham,y=Glasgow),size=0.5)
+tmpL <- data_mod_Lap[,c(Birm_index,Glasgow_index)]
+names(tmpL) <- names(df_sites)[1:2]
+p2 <- ggplot(tmpL) + geom_point(aes(x=Birmingham,y=Glasgow),size=0.5)
+p <- grid.arrange(p1,p2,ncol=2)
+ggsave(p,filename= "../Documents/Birmingham_Glasgow_original_Laplace.png",width=8,height=4)
+
+# 2. plot illustrating conditioning on a variable
+London_index <- find_site_index(site=London,grid_uk = xyUK20_sf)
+Inverness_index <- find_site_index(site=Inverness,grid_uk = xyUK20_sf)
+tmp <- data_mod_Lap[,c(Birm_index,Glasgow_index,Inverness_index)]
+names(tmp) <- names(df_sites)[c(1,3,4)]
+p1 <- ggplot(tmp) + geom_point(aes(x=London,y=Birmingham),size=0.5) + xlab(TeX("$Y_1$")) + ylab(TeX("$Y_2$")) + scale_color_manual(values = c("black", "#009ADA")) + theme(legend.position="none") + coord_fixed() + xlim(-10,10) + ylim(-10,10)
