196d06cb8c0ae158f1978959b36849732f00a2ca
Kika0 <kbratkov0@gmail.com>
2025-06-18 20:03:52 +0100

    Update plots on Laplace and original margins

--- EVA_presentation_plots.R
+++ EVA_presentation_plots.R
@@ -3,6 +3,8 @@ library(gridExtra)
 library(sf) # for handling spatial sf objects
 library(tidyverse)
 library(latex2exp) # latex expressions for plot labels
+library(texmex) # for pollution data
+
 theme_set(theme_bw())
 theme_replace(
   panel.spacing = unit(2, "lines"),
@@ -47,4 +49,56 @@ London_index <- find_site_index(site=London,grid_uk = xyUK20_sf)
 Inverness_index <- find_site_index(site=Inverness,grid_uk = xyUK20_sf)
 tmp <- data_mod_Lap[,c(Birm_index,Glasgow_index,Inverness_index)]
 names(tmp) <- names(df_sites)[c(1,3,4)]
-p1 <- ggplot(tmp) + geom_point(aes(x=London,y=Birmingham),size=0.5) + xlab(TeX("$Y_1$")) + ylab(TeX("$Y_2$")) + scale_color_manual(values = c("black", "#009ADA")) + theme(legend.position="none") + coord_fixed() + xlim(-10,10) + ylim(-10,10)
+q <- 0.9
+tmp$tf <- (tmp$London>vL)
+vL <- quantile(tmp[,1],q)
+p1 <- ggplot(tmp) + geom_point(aes(x=London,y=Birmingham),size=0.5) + xlab(TeX("$Y_1$")) + ylab(TeX("$Y_2$")) + scale_color_manual(values = c("black", "#009ADA")) + theme(legend.position="none") + coord_fixed() + xlim(-10,10) + ylim(-10,10) 
+p2 <- ggplot(tmp) + geom_point(aes(x=London,y=Inverness),size=0.5) + xlab(TeX("$Y_1$")) + ylab(TeX("$Y_3$")) + scale_color_manual(values = c("black", "#009ADA")) + theme(legend.position="none") + coord_fixed() + xlim(-10,10) + ylim(-10,10)  
+p1n <- ggplot(tmp) + geom_point(aes(x=London,y=Birmingham,col=tf),size=0.5) + xlab(TeX("$Y_1$")) + ylab(TeX("$Y_2$")) + scale_color_manual(values = c("black", "#009ADA")) + theme(legend.position="none") + coord_fixed() + xlim(-10,10) + ylim(-10,10) +geom_vline(xintercept=vL,color="#009ADA",linetype="dashed")
+p2n <- ggplot(tmp) + geom_point(aes(x=London,y=Inverness,col=tf),size=0.5) + xlab(TeX("$Y_1$")) + ylab(TeX("$Y_3$")) + scale_color_manual(values = c("black", "#009ADA")) + theme(legend.position="none") + coord_fixed() + xlim(-10,10) + ylim(-10,10) +geom_vline(xintercept=vL,color="#009ADA",linetype="dashed") 
+p <- grid.arrange(p1,p2,ncol=2)
+ggsave(p,filename="../Documents/condmodel_illustration.png",width=8,height=4)
+pn <- grid.arrange(p1n,p2n,ncol=2)
+ggsave(pn,filename="../Documents/condmodel_illustrationvL.png",width=8,height = 4)
+
+# 3. update density plot
+v <- 0.7
+j <- 1
+# transform to Laplace margins
+summer_lap <- as.data.frame((summer %>% apply(c(2),FUN=row_number))/(nrow(summer)+1)) %>% apply(c(1,2),FUN=unif_laplace_pit) %>% as.data.frame()
+pes <-  par_est(df=summer_lap,v=v,given=j,margin = "Normal", method = "sequential2")
+obsress <- observed_residuals(df = summer_lap,given = j,v = v,a = pes$a,b=pes$b) 
+x <- obsress[,1]
+
+# calculate parameters of each method
+margin <- c("GenGaus","AGGdelta","AGGsig","AGG")
+tr1 <- data.frame(y=as.numeric(),x=as.numeric(),Method=as.character())
+for (i in 1:5) {
+  methodpar <- as.numeric(res_margin_par_est(x,method=margin[i]))[-1]
+  tr1 <- rbind(tr1,data.frame(y=AGG_density(x=x,theta = methodpar),x=x,Method=margin[i]))
+}
+
+pl <- ggplot(data.frame(x=Z2)) + geom_density(aes(x=x),linetype="dashed")
+pl1 <- pl + geom_line(data=tr1 %>% mutate(Method_optim=factor(Method)),aes(x=x,y=y,col=Method)) +
+  xlab("Observed residuals kernel smoothed density") + ylab("Density") + ggtitle("Kernel smoothed residual density and AGG fits")+ scale_color_manual(values=c("Normal"="#070707","GenGaus"="#C11432","AGGdelta"="#009ADA","AGGsig"="#66A64F","AGG"="#FDD10A")) + ylim(c(0,0.5))
+ggsave(filename = "../Documents/AGG/density0.png",plot=pl1,height=5,width=8)
+
+
+pl1 <- pl + geom_line(data=tr1 %>% mutate(Method_optim=factor(Method)) %>% filter(Method %in% c()),aes(x=x,y=y,col=Method)) +
+  xlab("Observed residuals kernel smoothed density") + ylab("Density") + ggtitle("Kernel smoothed residual density and AGG fits")+ scale_color_manual(values=c("Normal"="#070707","GenGaus"="#C11432","AGGdelta"="#009ADA","AGGsig"="#66A64F","AGG"="#FDD10A"),drop=FALSE) + ylim(c(0,0.5))
+ggsave(filename = "../Documents/AGG/density1.png",plot=pl1,height=5,width=8)
+
+pl1 <- pl + geom_line(data=tr1 %>% mutate(Method_optim=factor(Method)) %>% filter(Method %in% c("Normal")),aes(x=x,y=y,col=Method)) +
+  xlab("Observed residuals kernel smoothed density") + ylab("Density") + ggtitle("Kernel smoothed residual density and AGG fits")+ scale_color_manual(values=c("Normal"="#070707","GenGaus"="#C11432","AGGdelta"="#009ADA","AGGsig"="#66A64F","AGG"="#FDD10A"),drop=FALSE) + ylim(c(0,0.5))
+ggsave(filename = "../Documents/AGG/density2.png",plot=pl1,height=5,width=8)
+
+pl1 <- pl + geom_line(data=tr1 %>% mutate(Method_optim=factor(Method))%>% filter(Method %in% c("Normal","GenGaus")),aes(x=x,y=y,col=Method)) +
+  xlab("Observed residuals kernel smoothed density") + ylab("Density") + ggtitle("Kernel smoothed residual density and AGG fits")+ scale_color_manual(values=c("Normal"="#070707","GenGaus"="#C11432","AGGdelta"="#009ADA","AGGsig"="#66A64F","AGG"="#FDD10A")) + ylim(c(0,0.5))
+ggsave(filename = "../Documents/AGG/density3.png",plot=pl1,height=5,width=8)
+
+pl1 <- pl + geom_line(data=tr1 %>% mutate(Method_optim=factor(Method))%>% filter(Method %in% c("Normal","GenGaus","AGGdelta")),aes(x=x,y=y,col=Method)) +
+  xlab("Observed residuals kernel smoothed density") + ylab("Density") + ggtitle("Kernel smoothed residual density and AGG fits")+ scale_color_manual(values=c("Normal"="#070707","GenGaus"="#C11432","AGGdelta"="#009ADA","AGGsig"="#66A64F","AGG"="#FDD10A")) + ylim(c(0,0.5))
+ggsave(filename = "../Documents/AGG/density4.png",plot=pl1,height=5,width=8)
+
+# 1. temperature large at Birmingham
+data_mod[data_mod]
