2298ef2d33b504c569c51ae4a762ce8985c73a31
Bratkova <bratkova@lancaster.ac.uk>
2025-06-19 08:08:03 +0100

    Add res margin density illustration for summer observed residuals

--- EVA_presentation_plots.R
+++ EVA_presentation_plots.R
@@ -68,17 +68,38 @@ j <- 1
 summer_lap <- as.data.frame((summer %>% apply(c(2),FUN=row_number))/(nrow(summer)+1)) %>% apply(c(1,2),FUN=unif_laplace_pit) %>% as.data.frame()
 pes <-  par_est(df=summer_lap,v=v,given=j,margin = "Normal", method = "sequential2")
 obsress <- observed_residuals(df = summer_lap,given = j,v = v,a = pes$a,b=pes$b) 
-x <- obsress[,1]
+Z2 <- as.numeric(unlist(obsress[,1]))
 
 # calculate parameters of each method
-margin <- c("GenGaus","AGGdelta","AGGsig","AGG")
+L1 <- res_margin_par_est(obs_res = obsress,method="Normal")
+L2 <- res_margin_par_est(obs_res = obsress,method="GenGaus")
+L3 <- res_margin_par_est(obs_res = obsress,method="AGGdelta")
+L4 <- res_margin_par_est(obs_res = obsress,method="AGGsig")
+L5 <- res_margin_par_est(obs_res = obsress,method="AGG")
+
+d <- ncol(sims)
+L1 <- L1 %>% mutate(method="Normal",AIC=2*likres+2*2) %>% mutate(res=c(1:d)[-j]) %>% mutate(mu_agg=mu,sigl=sig,sigu=sig,deltal=2,deltau=2) %>% dplyr::select(c(likres,mu_agg,sigl,sigu,deltal,deltau,AIC,method,res))
+L2 <- L2 %>% mutate(method="GenGaus",AIC=2*likres+2*3) %>% mutate(res=c(1:d)[-j]) %>% mutate(sigl=sig_agg,sigu=sig_agg,deltal=delta,deltau=delta) %>% dplyr::select(c(likres,mu_agg,sigl,sigu,deltal,deltau,AIC,method,res))
+L3 <- L3 %>% mutate(method="AGGdelta",AIC=2*likres+2*4) %>% mutate(res=c(1:d)[-j]) %>% mutate(sigl=sig_agg,sigu=sig_agg) %>% dplyr::select(c(likres,mu_agg,sigl,sigu,deltal,deltau,AIC,method,res))
+L4 <- L4 %>% mutate(method="AGGsig",AIC=2*likres+2*4) %>% mutate(res=c(1:d)[-j]) %>% mutate(deltal=delta,deltau=delta) %>% dplyr::select(c(likres,mu_agg,sigl,sigu,deltal,deltau,AIC,method,res))
+L5 <- L5 %>% mutate(method="AGG",AIC=2*likres+2*5) %>% mutate(res=c(1:d)[-j]) %>% dplyr::select(c(likres,mu_agg,sigl,sigu,deltal,deltau,AIC,method,res))
+
+tmp <- rbind(L1,L2,L3,L4,L5) %>% mutate(given=j) %>%  mutate(method=factor(method),given=factor(as.character(given)),res=factor(as.character(res)))
+
+# explore fits in the first iteration
+tmp1giv2 <- tmp %>% filter(given=="1",res=="2")
+methods <- unique(tmp$method)
 tr1 <- data.frame(y=as.numeric(),x=as.numeric(),Method=as.character())
+nllv <- c()
+method_par <- data.frame("Normal" = c(as.numeric(tmp1giv2[1,2:6])),"GenGaus" = c(as.numeric(tmp1giv2[2,2:6])),"AGGdelta" = c(as.numeric(tmp1giv2[3,2:6])),"AGGsig" = c(as.numeric(tmp1giv2[4,2:6])),"AGG" = c(as.numeric(tmp1giv2[5,2:6])))
+
+x <- seq(min(obsr[,1])-1,max(obsr[,1])+1,by=0.1)
 for (i in 1:5) {
-  methodpar <- as.numeric(res_margin_par_est(x,method=margin[i]))[-1]
-  tr1 <- rbind(tr1,data.frame(y=AGG_density(x=x,theta = methodpar),x=x,Method=margin[i]))
+  methodpar <- as.numeric(method_par[,i])
+  tr1 <- rbind(tr1,data.frame(y=AGG_density(x=x,theta = methodpar),x=x,Method=names(method_par[i])))
 }
 
-pl <- ggplot(data.frame(x=Z2)) + geom_density(aes(x=x),linetype="dashed")
+pl <- ggplot(data.frame(x=Z2)) + geom_density(aes(x=Z2),linetype="dashed") + xlim(c(min(obsr[,1])-1,max(obsr[,1])+1))
 pl1 <- pl + geom_line(data=tr1 %>% mutate(Method_optim=factor(Method)),aes(x=x,y=y,col=Method)) +
   xlab("Observed residuals kernel smoothed density") + ylab("Density") + ggtitle("Kernel smoothed residual density and AGG fits")+ scale_color_manual(values=c("Normal"="#070707","GenGaus"="#C11432","AGGdelta"="#009ADA","AGGsig"="#66A64F","AGG"="#FDD10A")) + ylim(c(0,0.5))
 ggsave(filename = "../Documents/AGG/density0.png",plot=pl1,height=5,width=8)
@@ -99,6 +120,11 @@ ggsave(filename = "../Documents/AGG/density3.png",plot=pl1,height=5,width=8)
 pl1 <- pl + geom_line(data=tr1 %>% mutate(Method_optim=factor(Method))%>% filter(Method %in% c("Normal","GenGaus","AGGdelta")),aes(x=x,y=y,col=Method)) +
   xlab("Observed residuals kernel smoothed density") + ylab("Density") + ggtitle("Kernel smoothed residual density and AGG fits")+ scale_color_manual(values=c("Normal"="#070707","GenGaus"="#C11432","AGGdelta"="#009ADA","AGGsig"="#66A64F","AGG"="#FDD10A")) + ylim(c(0,0.5))
 ggsave(filename = "../Documents/AGG/density4.png",plot=pl1,height=5,width=8)
+
+pl1 <- pl + geom_line(data=tr1 %>% mutate(Method_optim=factor(Method))%>% filter(Method %in% c("Normal","GenGaus","AGGdelta","AGGsig")),aes(x=x,y=y,col=Method)) +
+  xlab("Observed residuals kernel smoothed density") + ylab("Density") + ggtitle("Kernel smoothed residual density and AGG fits")+ scale_color_manual(values=c("Normal"="#070707","GenGaus"="#C11432","AGGdelta"="#009ADA","AGGsig"="#66A64F","AGG"="#FDD10A")) + ylim(c(0,0.5))
+ggsave(filename = "../Documents/AGG/density5.png",plot=pl1,height=5,width=8)
+
 
 # 1. temperature large at Birmingham
 data_mod[data_mod]
