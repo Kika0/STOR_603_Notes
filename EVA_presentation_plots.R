cdfe2735c4d3fa4db5a42e1ff367fa4f9302a8f9
Bratkova <bratkova@lancaster.ac.uk>
2025-06-25 15:24:34 +0100

    Update plot labels

--- EVA_presentation_plots.R
+++ EVA_presentation_plots.R
@@ -65,12 +65,31 @@ ggsave(pn,filename="../Documents/condmodel_illustrationvL.png",width=8,height = 
 
 # 3a. update density plot -------
 v <- 0.7
-j <- 2 # change j to repeat analysis cond. on other variables
+j <- 4 # change j to repeat analysis cond. on other variables
+
 # transform to Laplace margins
 summer_lap <- as.data.frame((summer %>% apply(c(2),FUN=row_number))/(nrow(summer)+1)) %>% apply(c(1,2),FUN=unif_laplace_pit) %>% as.data.frame()
 pes <-  par_est(df=summer_lap,v=v,given=j,margin = "Normal", method = "sequential2")
 obsress <- observed_residuals(df = summer_lap,given = j,v = v,a = pes$a,b=pes$b) 
 Z2 <- as.numeric(unlist(obsress[,1]))
+
+# illustrate data
+winter_lap <- as.data.frame((winter %>% apply(c(2),FUN=row_number))/(nrow(winter)+1)) %>% apply(c(1,2),FUN=unif_laplace_pit) %>% as.data.frame()
+vL <- quantile(winter$PM10,0.7)
+tmp <- winter %>% mutate(above_thres= as.character(winter$PM10>vL))
+vL1 <- quantile(summer$PM10,0.7)
+tmp1 <- summer %>% mutate(above_thres= as.character(summer$PM10>vL1))
+p <- grid.arrange(ggplot(tmp) + geom_point(aes(x=PM10,y=O3,col=above_thres),size=0.5,alpha=0.5) +
+               scale_color_manual(values = c("FALSE"="black","TRUE" = "#009ADA")) +
+               geom_vline(xintercept=vL,color="#009ADA",linetype="dashed") +
+               xlab(TeX("$PM_{10}$")) + ylab(TeX("$O_3$"))+
+               theme(legend.position = "none") ,
+             ggplot(tmp1) + geom_point(aes(x=PM10,y=O3,col=above_thres),size=0.5,alpha=0.5) +
+               scale_color_manual(values = c("FALSE"="black","TRUE" = "#C11432")) +
+               geom_vline(xintercept=vL1,color="#C11432",linetype="dashed") +
+               xlab(TeX("$PM_{10}$")) + ylab(TeX("$O_3$")) +
+               theme(legend.position="none") ,ncol=2)
+ggsave(p,filename = "../Documents/PM10_O3_illustrate.png",height=3,width=6.5)
 
 # calculate parameters of each method
 L1 <- res_margin_par_est(obs_res = obsress,method="Normal")
@@ -79,7 +98,7 @@ L3 <- res_margin_par_est(obs_res = obsress,method="AGGdelta")
 L4 <- res_margin_par_est(obs_res = obsress,method="AGGsig")
 L5 <- res_margin_par_est(obs_res = obsress,method="AGG")
 
-d <- ncol(sims)
+d <- ncol(winter)
 L1 <- L1 %>% mutate(method="Normal",AIC=2*likres+2*2) %>% mutate(res=c(1:d)[-j]) %>% mutate(mu_agg=mu,sigl=sig,sigu=sig,deltal=2,deltau=2) %>% dplyr::select(c(likres,mu_agg,sigl,sigu,deltal,deltau,AIC,method,res))
 L2 <- L2 %>% mutate(method="GenGaus",AIC=2*likres+2*3) %>% mutate(res=c(1:d)[-j]) %>% mutate(sigl=sig_agg,sigu=sig_agg,deltal=delta,deltau=delta) %>% dplyr::select(c(likres,mu_agg,sigl,sigu,deltal,deltau,AIC,method,res))
 L3 <- L3 %>% mutate(method="AGGdelta",AIC=2*likres+2*4) %>% mutate(res=c(1:d)[-j]) %>% mutate(sigl=sig_agg,sigu=sig_agg) %>% dplyr::select(c(likres,mu_agg,sigl,sigu,deltal,deltau,AIC,method,res))
@@ -89,19 +108,21 @@ L5 <- L5 %>% mutate(method="AGG",AIC=2*likres+2*5) %>% mutate(res=c(1:d)[-j]) %>
 tmp <- rbind(L1,L2,L3,L4,L5) %>% mutate(given=j) %>%  mutate(method=factor(method),given=factor(as.character(given)),res=factor(as.character(res)))
 
 # explore fits in the first iteration
-tmp1giv2 <- tmp %>% filter(given==as.character(j),res==as.character(1))
+if (j==1) {pickres <- 2} else {pickres <- 1}
+tmp1giv2 <- tmp %>% filter(given==as.character(j),res==as.character(pickres))
 methods <- unique(tmp$method)
 tr1 <- data.frame(y=as.numeric(),x=as.numeric(),Method=as.character())
 nllv <- c()
 method_par <- data.frame("Normal" = c(as.numeric(tmp1giv2[1,2:6])),"GenGaus" = c(as.numeric(tmp1giv2[2,2:6])),"AGGdelta" = c(as.numeric(tmp1giv2[3,2:6])),"AGGsig" = c(as.numeric(tmp1giv2[4,2:6])),"AGG" = c(as.numeric(tmp1giv2[5,2:6])))
 
-x <- seq(min(obsr[,1])-1,max(obsr[,1])+1,by=0.1)
+Z <- as.numeric(unlist(obsress[,1]))
+x <- seq(min(Z)-1,max(Z)+1,by=0.1)
 for (i in 1:5) {
   methodpar <- as.numeric(method_par[,i])
   tr1 <- rbind(tr1,data.frame(y=AGG_density(x=x,theta = methodpar),x=x,Method=names(method_par[i])))
 }
 
-pl <- ggplot(data.frame(x=Z2)) + geom_density(aes(x=Z2),linetype="dashed") + xlim(c(min(obsr[,1])-1,max(obsr[,1])+1))
+pl <- ggplot(data.frame(x=Z)) + geom_density(aes(x=x),linetype="dashed") + xlim(c(min(Z)-1,max(Z)+1))
 pl1 <- pl + geom_line(data=tr1 %>% mutate(Method_optim=factor(Method)),aes(x=x,y=y,col=Method)) +
   xlab("Observed residuals kernel smoothed density") + ylab("Density") + ggtitle("Kernel smoothed residual density and AGG fits")+ scale_color_manual(values=c("Normal"="#070707","GenGaus"="#C11432","AGGdelta"="#009ADA","AGGsig"="#66A64F","AGG"="#FDD10A")) + ylim(c(0,0.5))
 ggsave(filename = paste0("../Documents/AGG/density0_cond",j,".png"),plot=pl1,height=5,width=8)
@@ -145,7 +166,7 @@ for (i in 2:Nrep) {
   L4 <- res_margin_par_est(obs_res = obsr,method="AGGsig")
   L5 <- res_margin_par_est(obs_res = obsr,method="AGG")
   
-  d <- ncol(sims)
+  d <- ncol(summer)
   L1 <- L1 %>% mutate(method="Normal",AIC=2*likres+2*2) %>% mutate(res=c(1:d)[-j]) %>% mutate(mu_agg=mu,sigl=sig,sigu=sig,deltal=2,deltau=2) %>% dplyr::select(c(likres,mu_agg,sigl,sigu,deltal,deltau,AIC,method,res))
   L2 <- L2 %>% mutate(method="GenGaus",AIC=2*likres+2*3) %>% mutate(res=c(1:d)[-j]) %>% mutate(sigl=sig_agg,sigu=sig_agg,deltal=delta,deltau=delta) %>% dplyr::select(c(likres,mu_agg,sigl,sigu,deltal,deltau,AIC,method,res))
   L3 <- L3 %>% mutate(method="AGGdelta",AIC=2*likres+2*4) %>% mutate(res=c(1:d)[-j]) %>% mutate(sigl=sig_agg,sigu=sig_agg) %>% dplyr::select(c(likres,mu_agg,sigl,sigu,deltal,deltau,AIC,method,res))
@@ -156,12 +177,15 @@ for (i in 2:Nrep) {
 tmp <- rbind(tmp,tmp1)
 }
 
-plottitle <- TeX("Conditioning on $O_3> F^{-1}_{O_3} (0.7)$")
+pollutant_names <- c("O_3","NO_2","NO","SO_2","PM_{10}")
+label_residual_pollutant <- c(TeX("$O_3$"),TeX("$NO_2$"),TeX("$NO$"),TeX("$SO_2$"),TeX("$PM_{10}$"))
+ylims <- c(min(tmp$AIC),max(tmp$AIC))
+plottitle <- TeX(paste0("Conditioning on $",pollutant_names[j],"> F^{-1}_{",pollutant_names[j],"} (0.7)$"))
 # do a series of plots for the presentation
-pAIC1 <- ggplot(tmp %>% filter(given=="1") %>% filter(res %in% c("2"))) + geom_boxplot(aes(y=AIC,x=res,col=method)) +  scale_color_manual(values=c("Normal"="#070707","GenGaus"="#C11432","AGGdelta"="#009ADA","AGGsig"="#66A64F","AGG"="#FDD10A"),drop=FALSE) + xlab("Residuals") + ggtitle(plottitle) + scale_x_discrete(labels=c(TeX("$NO_2$"),TeX("$NO$"),TeX("$SO_2$"),TeX("$PM_{10}$")),drop=FALSE) + ylim(c(500,750))
-pAIC2 <- ggplot(tmp %>% filter(given=="1") %>% filter(res %in% c("2","3"))) + geom_boxplot(aes(y=AIC,x=res,col=method)) +  scale_color_manual(values=c("Normal"="#070707","GenGaus"="#C11432","AGGdelta"="#009ADA","AGGsig"="#66A64F","AGG"="#FDD10A"),drop=FALSE) + xlab("Residuals") + ggtitle(plottitle) + scale_x_discrete(labels=c(TeX("$NO_2$"),TeX("$NO$"),TeX("$SO_2$"),TeX("$PM_{10}$")),drop=FALSE) + ylim(c(500,750))
-pAIC3 <- ggplot(tmp %>% filter(given=="1") %>% filter(res%in% c("2","3","4"))) + geom_boxplot(aes(y=AIC,x=res,col=method)) +  scale_color_manual(values=c("Normal"="#070707","GenGaus"="#C11432","AGGdelta"="#009ADA","AGGsig"="#66A64F","AGG"="#FDD10A"),drop=FALSE) + xlab("Residuals") + ggtitle(plottitle) + scale_x_discrete(labels=c(TeX("$NO_2$"),TeX("$NO$"),TeX("$SO_2$"),TeX("$PM_{10}$")),drop=FALSE) + ylim(c(500,750))
-pAIC4 <- ggplot(tmp %>% filter(given=="1")) + geom_boxplot(aes(y=AIC,x=res,col=method)) +  scale_color_manual(values=c("Normal"="#070707","GenGaus"="#C11432","AGGdelta"="#009ADA","AGGsig"="#66A64F","AGG"="#FDD10A")) + xlab("Residuals") + ggtitle(plottitle) + scale_x_discrete(labels=c(TeX("$NO_2$"),TeX("$NO$"),TeX("$SO_2$"),TeX("$PM_{10}$")),drop=FALSE) + ylim(c(500,750))
+pAIC1 <- ggplot(tmp %>% filter(given==as.character(j)) %>% filter(res %in% as.character(c(1:d)[-j][1]))) + geom_boxplot(aes(y=AIC,x=res,col=method)) +  scale_color_manual(values=c("Normal"="#070707","GenGaus"="#C11432","AGGdelta"="#009ADA","AGGsig"="#66A64F","AGG"="#FDD10A"),drop=FALSE) + xlab("Residuals") + ggtitle(plottitle) + scale_x_discrete(labels=label_residual_pollutant[-j],drop=FALSE) + ylim(ylims)
+pAIC2 <- ggplot(tmp %>% filter(given==as.character(j)) %>% filter(res %in% as.character(c(1:d)[-j][1:2]))) + geom_boxplot(aes(y=AIC,x=res,col=method)) +  scale_color_manual(values=c("Normal"="#070707","GenGaus"="#C11432","AGGdelta"="#009ADA","AGGsig"="#66A64F","AGG"="#FDD10A"),drop=FALSE) + xlab("Residuals") + ggtitle(plottitle) + scale_x_discrete(labels=label_residual_pollutant[-j],drop=FALSE) + ylim(ylims)
+pAIC3 <- ggplot(tmp %>% filter(given==as.character(j)) %>% filter(res%in%  as.character(c(1:d)[-j][1:3]))) + geom_boxplot(aes(y=AIC,x=res,col=method)) +  scale_color_manual(values=c("Normal"="#070707","GenGaus"="#C11432","AGGdelta"="#009ADA","AGGsig"="#66A64F","AGG"="#FDD10A"),drop=FALSE) + xlab("Residuals") + ggtitle(plottitle) + scale_x_discrete(labels=label_residual_pollutant[-j],drop=FALSE) + ylim(ylims)
+pAIC4 <- ggplot(tmp %>% filter(given==as.character(j))) + geom_boxplot(aes(y=AIC,x=res,col=method)) +  scale_color_manual(values=c("Normal"="#070707","GenGaus"="#C11432","AGGdelta"="#009ADA","AGGsig"="#66A64F","AGG"="#FDD10A")) + xlab("Residuals") + ggtitle(plottitle) + scale_x_discrete(labels=label_residual_pollutant[-j],drop=FALSE) + ylim(ylims)
 ggsave(filename = paste0("../Documents/AGG/AICZ2_cond",j,".png"),plot=pAIC1,height=5,width=6)
 ggsave(filename = paste0("../Documents/AGG/AICZ3_cond",j,".png"),plot=pAIC2,height=5,width=6)
 ggsave(filename = paste0("../Documents/AGG/AICZ4_cond",j,".png"),plot=pAIC3,height=5,width=6)
@@ -173,49 +197,50 @@ GenGaus <- c()
 AGGsig <- c()
 AGGdelta <- c()
 AGGsigdelta <- c()
-res <- rep(c("Z2","Z3","Z4","Z5"),length(L1))
+res <- rep(paste0("Z",c(1:d)[-j]),length(L1))
 for (i in 1:length(L1)) {
   GenGaus <- tmp %>% filter(method=="GenGaus") %>% pull(likres)
   AGGsig <- tmp %>% filter(method=="AGGsig") %>% pull(likres)  
   AGGdelta <- tmp %>% filter(method=="AGGdelta") %>% pull(likres)
   AGGsigdelta <- tmp %>% filter(method=="AGG") %>% pull(likres)
 }
-AGG_GG <- 2*((tmp %>% filter(method=="GenGaus",given=="1") %>% pull(likres))-(tmp %>% filter(method=="AGG",given=="1") %>% pull(likres)) )
-tmpagg <- data.frame(AGG_GG=AGG_GG,res=tmp %>% filter(method=="GenGaus",given=="1") %>% pull(res),given=tmp %>% filter(method=="GenGaus",given=="1") %>% pull(given))
+
+AGG_GG <- 2*((tmp %>% filter(method=="GenGaus",given==as.character(j)) %>% pull(likres))-(tmp %>% filter(method=="AGG",given==as.character(j)) %>% pull(likres)) )
+AGGsig_GG <- 2*((tmp %>% filter(method=="GenGaus",given==as.character(j)) %>% pull(likres))-(tmp %>% filter(method=="AGGsig",given==as.character(j)) %>% pull(likres)) )
+AGGdelta_GG <- 2*((tmp %>% filter(method=="GenGaus",given==as.character(j)) %>% pull(likres))-(tmp %>% filter(method=="AGGdelta",given==as.character(j)) %>% pull(likres)) )
+tmpagg <- data.frame(AGG_GG=AGG_GG,res=tmp %>% filter(method=="GenGaus",given==as.character(j)) %>% pull(res),given=tmp %>% filter(method=="GenGaus",given==as.character(j)) %>% pull(given))
+ylims <- c(0,ceiling(max(c(AGG_GG,AGGsig_GG,AGGdelta_GG))))
 p1 <- ggplot(tmpagg,aes(x=res,y=AGG_GG))+
   geom_boxplot() + xlab("Residuals") +
   # ylab("Different scale vs shape")
-  ylab(TeX("$2(l_{AGG}(\\hat{\\mu},\\hat{\\sigma}_l,\\hat{\\sigma}_u,\\hat{\\delta}_l,\\hat{\\delta}_u)-l_{GG}(\\hat{\\mu},\\hat{\\sigma},\\hat{\\delta}))$")) + ylim(c(0,max(2*(AGGsigdelta-GenGaus)))) +
-  geom_hline(yintercept = qchisq(0.95,2),linetype="dashed",col = "#66A64F") +  geom_hline(yintercept = qchisq(0.99,2),linetype="dashed", col= "#FDD10A") + scale_x_discrete(labels=c(TeX("$NO_2$"),TeX("$NO$"),TeX("$SO_2$"),TeX("$PM_{10}$")),drop=FALSE)
+  ylab(TeX("$2(l_{AGG}(\\hat{\\mu},\\hat{\\sigma}_l,\\hat{\\sigma}_u,\\hat{\\delta}_l,\\hat{\\delta}_u)-l_{GG}(\\hat{\\mu},\\hat{\\sigma},\\hat{\\delta}))$")) + ylim(ylims) +
+  geom_hline(yintercept = qchisq(0.95,2),linetype="dashed",col = "#66A64F") +  geom_hline(yintercept = qchisq(0.99,2),linetype="dashed", col= "#FDD10A") + scale_x_discrete(labels=label_residual_pollutant[-j],drop=FALSE)
 
-AGGsig_GG <- 2*((tmp %>% filter(method=="GenGaus",given=="1") %>% pull(likres))-(tmp %>% filter(method=="AGGsig",given=="1") %>% pull(likres)) )
-tmpagg <- data.frame(AGG_GG=AGGsig_GG,res=tmp %>% filter(method=="GenGaus",given=="1") %>% pull(res),given=tmp %>% filter(method=="AGGsig",given=="1") %>% pull(given))
+tmpagg <- data.frame(AGG_GG=AGGsig_GG,res=tmp %>% filter(method=="GenGaus",given==as.character(j)) %>% pull(res),given=tmp %>% filter(method=="AGGsig",given==as.character(j)) %>% pull(given))
 p2 <- ggplot(tmpagg,aes(x=res,y=AGG_GG))+ 
   geom_boxplot() + xlab("Residuals") +
   # ylab("Different scale vs shape")
-  ylab(TeX("$2(l_{AGG}(\\hat{\\mu},\\hat{\\sigma}_l,\\hat{\\sigma}_u,\\hat{\\delta}_c,\\hat{\\delta}_c)-l_{GG}(\\hat{\\mu},\\hat{\\sigma},\\hat{\\delta}))$"))  + ylim(c(0,max(2*(AGGsigdelta-GenGaus)))) +
-  geom_hline(yintercept = qchisq(0.95,2),linetype="dashed",col = "#66A64F") +  geom_hline(yintercept = qchisq(0.99,2),linetype="dashed", col= "#FDD10A") + scale_x_discrete(labels=c(TeX("$NO_2$"),TeX("$NO$"),TeX("$SO_2$"),TeX("$PM_{10}$")),drop=FALSE)
+  ylab(TeX("$2(l_{AGG}(\\hat{\\mu},\\hat{\\sigma}_l,\\hat{\\sigma}_u,\\hat{\\delta}_c,\\hat{\\delta}_c)-l_{GG}(\\hat{\\mu},\\hat{\\sigma},\\hat{\\delta}))$"))  + ylim(ylims) +
+  geom_hline(yintercept = qchisq(0.95,1),linetype="dashed",col = "#66A64F") +  geom_hline(yintercept = qchisq(0.99,1),linetype="dashed", col= "#FDD10A") + scale_x_discrete(labels=label_residual_pollutant[-j],drop=FALSE)
 
-AGGdelta_GG <- 2*((tmp %>% filter(method=="GenGaus",given=="1") %>% pull(likres))-(tmp %>% filter(method=="AGGdelta",given=="1") %>% pull(likres)) )
-tmpagg <- data.frame(AGG_GG=AGGdelta_GG,res=tmp %>% filter(method=="GenGaus",given=="1") %>% pull(res),given=tmp %>% filter(method=="GenGaus",given=="1") %>% pull(given))
+tmpagg <- data.frame(AGG_GG=AGGdelta_GG,res=tmp %>% filter(method=="GenGaus",given==as.character(j)) %>% pull(res),given=tmp %>% filter(method=="GenGaus",given==as.character(j)) %>% pull(given))
 p3 <- ggplot(tmpagg,aes(x=res,y=AGG_GG))+
   geom_boxplot() + xlab("Residuals") +
   # ylab("Different scale vs shape")
-  ylab(TeX("$2(l_{AGG}(\\hat{\\mu},\\hat{\\sigma}_c,\\hat{\\sigma}_c,\\hat{\\delta}_l,\\hat{\\delta}_u)-l_{GG}(\\hat{\\mu},\\hat{\\sigma},\\hat{\\delta}))$"))+ ylim(c(0,max(2*(AGGsigdelta-GenGaus)))) +
-  geom_hline(yintercept = qchisq(0.95,2),linetype="dashed",col = "#66A64F") +  geom_hline(yintercept = qchisq(0.99,2),linetype="dashed", col= "#FDD10A") + scale_x_discrete(labels=c(TeX("$NO_2$"),TeX("$NO$"),TeX("$SO_2$"),TeX("$PM_{10}$")),drop=FALSE)
+  ylab(TeX("$2(l_{AGG}(\\hat{\\mu},\\hat{\\sigma}_c,\\hat{\\sigma}_c,\\hat{\\delta}_l,\\hat{\\delta}_u)-l_{GG}(\\hat{\\mu},\\hat{\\sigma},\\hat{\\delta}))$"))+ ylim(ylims) +
+  geom_hline(yintercept = qchisq(0.95,1),linetype="dashed",col = "#66A64F") +  geom_hline(yintercept = qchisq(0.99,1),linetype="dashed", col= "#FDD10A") + scale_x_discrete(labels=label_residual_pollutant[-j],drop=FALSE)
 p <- grid.arrange(p1,p2,p3,ncol=3)
 
 ggsave(filename = paste0("../Documents/AGG/AGGmargin_likratiotest_cond",j,".png"),plot=p,height=3,width=8)
 
 # 3c. count the minima of each method
-label_legend <- c(TeX("$O_3$"),TeX("$NO_2$"),TeX("$NO$"),TeX("$SO_2$"),TeX("$PM_{10}$"))
 tmpc <- tmp %>% mutate(ite=rep(1:Nrep,each=20)) %>% group_by(ite,res) %>% slice(which.min(AIC))
-p <- ggplot(tmpc) + geom_bar(aes(x=method,fill=res)) +  scale_fill_manual(values=c("#C11432","#009ADA","#66A64F","#FDD10A"),labels=label_legend[-j],drop=FALSE) + labs(fill=c("Residual variable"))
+p <- ggplot(tmpc) + geom_bar(aes(x=method,fill=res)) +  scale_fill_manual(values=c("#C11432","#009ADA","#66A64F","#FDD10A"),labels=label_residual_pollutant[-j],drop=FALSE) + labs(fill=c("Residual variable"))
 ggsave(filename = paste0("../Documents/AGG/countAIC_cond",j,".png"),plot=p,height=3,width=5)
 
 # 3d. combine AGG methods
 tmpc <- tmpc %>% mutate(method=recode(method,AGG="AGG",AGGsig="AGG",AGGdelta="AGG")) 
-p <- ggplot(tmpc) + geom_bar(aes(x=method,fill=res)) +  scale_fill_manual(values=c("#C11432","#009ADA","#66A64F","#FDD10A"),labels=label_legend[-j],drop=FALSE) + labs(fill=c("Residual variable"))
+p <- ggplot(tmpc) + geom_bar(aes(x=method,fill=res)) +  scale_fill_manual(values=c("#C11432","#009ADA","#66A64F","#FDD10A"),labels=label_residual_pollutant[-j],drop=FALSE) + labs(fill=c("Residual variable"))
 ggsave(filename = paste0("../Documents/AGG/countAIC_AGGcond",j,".png"),plot=p,height=3,width=5)
 
 # 1. temperature large at Birmingham
