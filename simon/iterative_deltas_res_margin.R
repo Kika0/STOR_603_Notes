396177a6d62d5ce8f08643186f1517bf1b55baeb
Bratkova <bratkova@lancaster.ac.uk>
2025-08-06 11:12:04 +0100

    Explore conditioning on Birmingham for 20 iterations

---
+++ simon/iterative_deltas_res_margin.R
@@ -0,0 +1,131 @@
+res_margin_par_est_ite <- function(df=Z,given=cond_site,N=10, show_ite=FALSE,mu_init=NULL,sigl_init=NULL,sigu_init,deltal_init=NULL,deltau_init=NULL,method="onephi",SN=NULL, b_inc=FALSE)  {
+  d <- ncol(df)
+  nv <- nrow(df)
+  res <- 1:d
+  mu_agg <- sigl <- sigu <- deltal <- deltau <- data.frame(matrix(ncol=(N+1),nrow = d))
+  # calculate a with initial values for mu and sigma
+  if (is.numeric(mu_init) & is.numeric(sigl_init) & is.numeric(sigl_init)) {
+    mu_agg[,1] <- mu_init
+    sigl[,1] <- sigl_init
+    sigu[,1] <- sigu_init
+  } else {
+    mu_agg[,1] <- 0
+    sigl[,1] <- 1
+    sigu[,1] <- 1
+  }
+  if (is.numeric(deltau_init) & is.numeric(deltau_init)) {
+    deltal[,1] <- deltal_init
+    deltau[,1] <- deltau_init
+  } else {
+    deltal[,1] <- 2
+    deltau[,1] <- 2
+  }
+
+  for (i in 1:N) {
+    for (j in 1:d) {
+      Z2 <- as.numeric(unlist(Z[,j]))
+      opt <- optim(fn=NLL_AGG,x=Z2,deltal_hat=as.numeric(deltal[1,i]),deltau_hat=as.numeric(deltau[1,i]),par=c(mean(Z2),sd(Z2),sd(Z2)),control=list(maxit=2000),method = "Nelder-Mead")
+      mu_agg[j,i+1] <- opt$par[1]
+      sigl[j,i+1] <- opt$par[2]
+      sigu[j,i+1] <- opt$par[3]
+    }
+    # calculate deltal and deltau
+      opt <- optim(fn=NLL_AGG_deltas,df = Z,mu1=as.numeric(mu_agg[,i+1]),sigl1=as.numeric(sigl[,i+1]),sigu1=as.numeric(sigu[,i+1]),control=list(maxit=2000),par = c(as.numeric(deltal[1,i]),as.numeric(deltau[1,i])),method = "Nelder-Mead")
+      deltal[,i+1] <- opt$par[1]
+      deltau[,i+1] <- opt$par[2]
+  }
+  par_sum <- data.frame("mu_agg" = as.numeric(mu_agg[,N+1]),"sigl" = as.numeric(sigl[,N+1]),"sigu" = as.numeric(sigu[,N+1]),"deltal" = as.numeric(deltal[,N+1]), "deltau" = as.numeric(deltau[,N+1]))
+  if (show_ite == TRUE) {
+return(list(mu_agg,sigl,sigu,deltal,deltau,par_sum))
+  } else {return(par_sum)}
+}
+Nite <- 100
+tmp1 <- res_margin_par_est_ite(df=Z,show_ite = TRUE,N=Nite)
+
+# examine output
+
+# plot delta estimates
+tmp_delta <- rbind(data.frame(delta=as.numeric(unlist(tmp[[4]][1,])),iteration=1:(Nite+1),parameter = "delta_lower"),
+                   data.frame(delta=as.numeric(unlist(tmp[[5]][1,])),iteration=1:(Nite+1),parameter = "delta_upper"))
+pd <- ggplot(tmp_delta) + geom_point(aes(x=iteration,y=delta,col=parameter))
+ggsave(pd,filename="../Documents/iterative_deltas_res_margin/deltas.png")
+
+# plot across iterations for a selected site
+random_site <- 100
+tmp_all <- rbind(data.frame(delta=as.numeric(unlist(tmp[[1]][random_site,])),iteration=1:(Nite+1),parameter = "mu_agg"),
+                   data.frame(delta=as.numeric(unlist(tmp[[2]][random_site,])),iteration=1:(Nite+1),parameter = "sigma_lower"),
+                   data.frame(delta=as.numeric(unlist(tmp[[3]][random_site,])),iteration=1:(Nite+1),parameter = "sigma_upper"),
+                   data.frame(delta=as.numeric(unlist(tmp[[4]][random_site,])),iteration=1:(Nite+1),parameter = "delta_lower"),
+                   data.frame(delta=as.numeric(unlist(tmp[[5]][random_site,])),iteration=1:(Nite+1),parameter = "delta_upper")
+                   
+                   )
+# plot the final iteration
+p1 <- ggplot(tmp_all) + geom_point(aes(x=iteration,y=delta,col=parameter))
+ggsave(p1,filename="../Documents/iterative_deltas_res_margin/random_site_par.png")
+# map final iteration or several
+est_Birm <- est_all_sf %>% filter(cond_site=="Birmingham")
+est_ite <- tmp[[6]] %>% add_row(.before=find_site_index(Birmingham,grid_uk = xyUK20_sf))
+names(est_ite) <- paste0(names(est_ite),"_ite")
+tmpsf <- cbind(est_Birm,est_ite)
+
+t1 <- tmpsf %>% tm_shape() + tm_dots(col="mu_agg",palette="viridis")
+t2 <- tmpsf %>% tm_shape() + tm_dots(col="mu_agg_ite",palette="viridis")
+p1 <- tmap_arrange(t1,t2,ncol=2)
+tmap_save(p1,filename="../Documents/iterative_deltas_res_margin/mu_agg.png",width=8,height=3)
+
+t1 <- tmpsf %>% tm_shape() + tm_dots(col="sigl",palette="viridis")
+t2 <- tmpsf %>% tm_shape() + tm_dots(col="sigl_ite",palette="viridis")
+p1 <- tmap_arrange(t1,t2,ncol=2)
+tmap_save(p1,filename="../Documents/iterative_deltas_res_margin/sigma_lower.png",width=8,height=3)
+
+t1 <- tmpsf %>% tm_shape() + tm_dots(col="sigu",palette="viridis")
+t2 <- tmpsf %>% tm_shape() + tm_dots(col="sigu_ite",palette="viridis")
+p1 <- tmap_arrange(t1,t2,ncol=2)
+tmap_save(p1,filename="../Documents/iterative_deltas_res_margin/sigma_upper.png",width=8,height=3)
+
+
+
+library(tmap) # spatial map plots
+library(sf) # for handling spatial sf objects
+library(viridis)
+library(tidyverse)
+library(latex2exp) # latex expressions for plot labels
+theme_set(theme_bw())
+theme_replace(
+  panel.spacing = unit(2, "lines"),
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  strip.background = element_blank(),
+  panel.border = element_rect(colour = "black", fill = NA) )
+file.sources = list.files(pattern="*helpers.R")
+sapply(file.sources,source,.GlobalEnv)
+source("spatial_parameter_estimation.R") # for spatial_par_est function
+load("data_processed/temperature_data.RData")
+load("data_processed/spatial_helper.RData")
+Birmingham <- c(-1.9032,52.4806)
+Glasgow <- c(-4.258109,55.859112)
+London <- c(-0.127676,51.529972)
+Inverness <- c(-4.22498,57.48065) # Inverness bus station
+Lancaster <- c(-2.78440,54.00871) # PSC building Lancaster University
+Newcastle <- c(-1.61682,54.96902) # Newcastle railway station
+Cromer <- c(1.28486,53.05349)
+Hull <- c(-0.335827,53.767750)
+Lowestoft <- c(1.72431,52.48435)
+Truro <- c(-5.05125342465549,50.263075821232704)
+Dolgellau <- c(-3.8844362867080897,52.74213275545185)
+Bournemouth <- c(-1.8650607066137428,50.72173094587856)
+df_sites <- data.frame(Birmingham,Glasgow,London,Inverness,Lancaster,Newcastle,Cromer,Hull,Lowestoft,Truro,Dolgellau,Bournemouth)
+#spatial_par_est saves parameter estimates as est_all_sf sf object in ../Documents folder
+q <- 0.9 # quantile threshold
+# spatial_par_est(data_Lap = data_mod_Lap,cond_sites = df_sites,dayshift = 0,v=q,Ndays_season = 90,title = paste0("all12sites",q*100))
+# spatial_par_est(data_Lap = data_mod_Lap[1:3600,],cond_sites = df_sites,dayshift = 0,v=q,Ndays_season = 90,title = paste0("first40_12sites",q*100))
+# spatial_par_est(data_Lap = data_mod_Lap[5401:9000,],cond_sites = df_sites,dayshift = 0,v=q,Ndays_season = 90,title = paste0("last40_12sites",q*100))
+
+# load all three parameter estimates sf objects ----
+load(paste0("data_processed/N9000_sequential2_AGG_all12sites",q*100,".RData"))
+est_all <- as.data.frame(est_all_sf)
+
+# calculate the observed residuals (set Birmingham as conditioning site)
+cond_site <- find_site_index(as.numeric(df_sites[,1]),grid_uk = xyUK20_sf)
+Z <- observed_residuals(df = data_mod_Lap, given = cond_site, v = q,a= discard(as.numeric(est_all$a),is.na),b = discard(as.numeric(est_all$b),is.na))
+
