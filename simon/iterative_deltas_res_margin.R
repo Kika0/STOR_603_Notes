e3e80779b10c316e6027aca23502f11d6fbd1ba5
Kika0 <kbratkov0@gmail.com>
2025-09-03 19:14:48 +0100

    Add Leeds as a conditioning site

--- simon/iterative_deltas_res_margin.R
+++ simon/iterative_deltas_res_margin.R
@@ -70,7 +70,8 @@ Lowestoft <- c(1.72431,52.48435)
 Truro <- c(-5.05125342465549,50.263075821232704)
 Dolgellau <- c(-3.8844362867080897,52.74213275545185)
 Bournemouth <- c(-1.8650607066137428,50.72173094587856)
-df_sites <- data.frame(Birmingham,Glasgow,London,Inverness,Lancaster,Newcastle,Cromer,Hull,Lowestoft,Truro,Dolgellau,Bournemouth)
+Leeds <- c(-1.5410242288355958,53.80098118214994)
+df_sites <- data.frame(Birmingham,Glasgow,London,Inverness,Lancaster,Newcastle,Cromer,Hull,Lowestoft,Truro,Dolgellau,Bournemouth,Leeds)
 #spatial_par_est saves parameter estimates as est_all_sf sf object in ../Documents folder
 q <- 0.9 # quantile threshold
 # load all three parameter estimates sf objects
@@ -132,7 +133,7 @@ iter_delta_site <- function(j,Nite=50,sites=df_sites,grid=xyUK20_sf,data=data_mo
   est_site <- par_est %>% filter(cond_site==cond_site_name)
   cond_site_coord <- sites %>% dplyr::select(all_of(cond_site_name)) %>% pull()
   cond_site <- find_site_index(cond_site_coord,grid_uk = grid)
-  Z <- observed_residuals(df = data, given = cond_site, v = q,a= discard(as.numeric(est_site$a),is.na),b = discard(as.numeric(est_site$b),is.na))
+  Z <- observed_residuals(df = data_mod_Lap, given = cond_site, v = q,a= discard(as.numeric(est_site$a),is.na),b = discard(as.numeric(est_site$b),is.na))
   
   # estimate parameters iteratively --------------------------------------------
   tmp <- res_margin_par_est_ite(data=Z,show_ite = TRUE,N=Nite)
--- simon/iterative_sigmau.R
+++ simon/iterative_sigmau.R
@@ -68,7 +68,8 @@ Lowestoft <- c(1.72431,52.48435)
 Truro <- c(-5.05125342465549,50.263075821232704)
 Dolgellau <- c(-3.8844362867080897,52.74213275545185)
 Bournemouth <- c(-1.8650607066137428,50.72173094587856)
-df_sites <- data.frame(Birmingham,Glasgow,London,Inverness,Lancaster,Newcastle,Cromer,Hull,Lowestoft,Truro,Dolgellau,Bournemouth)
+Leeds <- c(-1.5410242288355958,53.80098118214994)
+df_sites <- data.frame(Birmingham,Glasgow,London,Inverness,Lancaster,Newcastle,Cromer,Hull,Lowestoft,Truro,Dolgellau,Bournemouth,Leeds)
 #spatial_par_est saves parameter estimates as est_all_sf sf object in ../Documents folder
 q <- 0.9 # quantile threshold
 # load all three parameter estimates sf objects
