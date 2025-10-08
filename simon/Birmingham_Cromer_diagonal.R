4816e92adee0d8e4e9cccd0b0e35b5df86980c10
Kika0 <kbratkov0@gmail.com>
2025-10-08 19:01:43 +0100

    Add plots comparing against 94marginal thresholds

--- simon/Birmingham_Cromer_diagonal.R
+++ simon/Birmingham_Cromer_diagonal.R
@@ -185,6 +185,9 @@ ggsave(p,filename=paste0("../Documents/Birmingham_Cromer_diagonal/sigl_distance_
 # try phi0,phi1,phi2,phi3 joint estimation
 load("data_processed/iterative_delta_estimates_Birmingham_Cromer_diagonal.RData")
 result <- sapply(1:length(sites_index_diagonal),FUN = iter_sigmas_site,sites = sites_index_diagonal,cond_site_names = site_name_diagonal,par_est = est_all_diag1,folder_name = "Birmingham_Cromer_diagonal/sigmas",simplify = FALSE)
+# save these estimates
+#save(result, file="data_processed/iterative_sigmas_estimates_Birmingham_Cromer_diagonal.RData")
+load("data_processed/iterative_sigmas_estimates_Birmingham_Cromer_diagonal.RData")
 
 # examine output
 phi0 <- sapply(1:length(sites_index_diagonal),FUN = function (i) as.numeric(st_drop_geometry( result[[i]][1,34])))
@@ -210,4 +213,44 @@ tmphi3 <- tm_shape(xyUK20_sf) + tm_dots() + tm_shape(est_phi) + tm_dots(fill="ph
 t <- tmap_arrange(tmphi0,tmphi1,tmphi2,tmphi3,ncol=4)
 tmap_save(t,filename=paste0("../Documents/Birmingham_Cromer_diagonal/phis_all4.png"),width=8,height=4)
 
+# plots against 94 marginal quantile
+load("data_processed/94thresholds.RData")
+temp94 <- tm_thres %>% filter(data_source=="CPM") %>% pull(temp)
+temp94diff <- tm_thres_diff %>% filter(data_source=="CPM_diff") %>% pull(temperature)
+data.frame(phi0,temp94=temp94[sites_index_diagonal]) %>%  ggplot() + geom_point(aes(x=temp94,y=phi0))
+data.frame(phi0,temp94diff=temp94diff[sites_index_diagonal]) %>%  ggplot() + geom_point(aes(x=temp94diff,y=phi0))
 
+# combine into one plot
+tmp <- data.frame(param_value=c(phi0,phi1,phi2,phi3),temp94=rep(temp94[sites_index_diagonal],4),phi = rep(c("phi0","phi1","phi2","phi3"),each=4))
+p <- ggplot(tmp) + geom_point(aes(x=temp94,y=param_value,col=phi)) + geom_line(aes(x=temp94,y=param_value,col=phi))
+ggsave(p,filename="../Documents/phis_temp94.png",width=5,height=5)
+tmp <- data.frame(param_value=c(phi0,phi1,phi2,phi3),temp94diff=rep(temp94diff[sites_index_diagonal],4),phi = rep(c("phi0","phi1","phi2","phi3"),each=4))
+p <- ggplot(tmp) + geom_point(aes(x=temp94diff,y=param_value,col=phi)) + geom_line(aes(x=temp94diff,y=param_value,col=phi))
+ggsave(p,filename="../Documents/phis_temp94diff.png",width=5,height=5)
+
+# iterative a,b,mu and 
+load("data_processed/iterative_sigmal_estimates_Birmingham_Cromer_diagonal.RData")
+sites = sites_index_diagonal
+cond_site_names = site_name_diagonal
+q <- 0.9
+if(is.null(cond_site_names)) {
+  cond_site_name <- names(sites)[j]
+  cond_site_names <- names(sites)
+} else {  cond_site_name <- cond_site_names[j] }
+
+if (is.numeric(sites)) {cond_site <- sites[j]} else{
+  cond_site_coord <- sites %>% dplyr::select(all_of(cond_site_name)) %>% pull()
+  cond_site <- find_site_index(cond_site_coord,grid_uk = grid)    }
+
+# calculate distance from the conditioning site
+dist_tmp <- as.numeric(unlist(st_distance(xyUK20_sf[cond_site,],xyUK20_sf)))
+# remove zero distance
+dist_tmp <- dist_tmp[dist_tmp>0]
+# normalise distance using a common constant
+distnorm <- dist_tmp/1000000
+parest_site <- st_drop_geometry(result[[j]]) %>% dplyr::select(sigl_ite_sigl,sigu_ite_sigu,deltal_ite,deltau_ite) %>% na.omit()
+
+
+try7 <- par_est_ite(dataLap=data_mod_Lap,given=cond_site,cond_site_dist=distnorm, parest_site = parest_site,Nite=5, show_ite=TRUE)
+
+# separate parameter estimation and analysis
