f6bb2bd713ac50131abdcdaf6e5446506f055f17
Kika0 <kbratkov0@gmail.com>
2026-07-20 11:21:54 +0100

    Add loess mean smoother regression line

--- simon/final_model_diagnostics.R
+++ simon/final_model_diagnostics.R
@@ -16,7 +16,7 @@ q <- 0.9 # set quantile threshold
 load(paste0("data_processed/N9000_sequential2_AGG_all12sites",q*100,".RData"),verbose = TRUE) # original parameter estimates
 
 # 1. plot of beta against latitude difference from the conditioning site ------
-plot_beta_latitude <- function(b,given,res,gridUK=xyUK20_sf,cond_site,folder_name,plot_name="beta_latitude_difference",comb_sites=FALSE) {
+plot_beta_latitude <- function(b,given,res,gridUK=xyUK20_sf,cond_site,folder_name,plot_name="beta_latitude_difference",comb_sites=FALSE,line_mean=FALSE) {
   stopifnot(length(b)==length(given))
 lat_diff <-   sapply(1:length(b),FUN=function(i) {
     as.numeric(gridUK$lat)[res[i]] - as.numeric(gridUK$lat)[given[i]]
@@ -40,6 +40,9 @@ ggsave(p,filename=paste0(folder_name,plot_name,".png"),width=10,height=7)
 }
 if (comb_sites==TRUE) {
   p <- ggplot(tmp) + geom_point(aes(x=lat_diff,y=b,col=cond_site),size=0.3) + scale_color_manual(values=c13) + labs(x="Latitude difference",y=TeX("$\\beta$"),col="Conditioning site") + theme(axis.title.y = element_text(angle = 0,vjust=0.5)) 
+  if (line_mean==TRUE) {
+    p <- p + geom_smooth(aes(y=b,x=lat_diff,col=cond_site),method="loess")
+  }
   ggsave(p,filename=paste0(folder_name,plot_name,".png"),width=5,height=4)
 }
 }
@@ -84,4 +87,9 @@ tmp_ec <- tmp %>% filter(cond_site %in% east_coast_sites)
 tmp_nec <- tmp %>% filter(!cond_site %in% east_coast_sites)
 plot_beta_latitude(b=tmp_ec$b_new,given=tmp_ec$given,res=tmp_ec$res,cond_site = tmp_ec$cond_site,folder_name = folder_name,plot_name = "beta_model_3_east_coast",comb_sites=TRUE)
 plot_beta_latitude(b=tmp_nec$b_new,given=tmp_nec$given,res=tmp_nec$res,cond_site = tmp_nec$cond_site,folder_name = folder_name,plot_name = "beta_model_3_not_east_coast",comb_sites=TRUE)
+
+# add regression lines
+plot_beta_latitude(b=tmp_ec$b_new,given=tmp_ec$given,res=tmp_ec$res,cond_site = tmp_ec$cond_site,folder_name = folder_name,plot_name = "beta_model_3_east_coast_mean_smooth",comb_sites=TRUE,line_mean = TRUE)
+plot_beta_latitude(b=tmp_nec$b_new,given=tmp_nec$given,res=tmp_nec$res,cond_site = tmp_nec$cond_site,folder_name = folder_name,plot_name = "beta_model_3_not_east_coast_mean_smooth",comb_sites=TRUE,line_mean=TRUE)
+
 
