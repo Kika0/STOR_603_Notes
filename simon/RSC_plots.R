aa1a086b0a571835aae71644d3c597665f0ae17b
Kika0 <kbratkov0@gmail.com>
2026-09-08 09:22:44 +0100

    Remove unused bits and tidy up

--- simon/RSC_plots.R
+++ simon/RSC_plots.R
@@ -14,7 +14,7 @@ theme_replace(
   panel.grid.minor = element_blank(),
   strip.background = element_blank(),
   panel.border = element_rect(colour = "black", fill = NA) )
-folder_name <- "../Documents/TakeAIM2026/"
+folder_name <- "../Documents/RSC2026/"
 
 # load observed data
 #source("spatial_parameter_estimation.R") # for spatial_par_est function
@@ -36,14 +36,6 @@ july3_1976 <- data01_obs %>% dplyr::filter(date_obs==lubridate::ymd("1976-07-03"
 which.min(abs(data01$time[data01$class=="mod"]- (data01_obs$time[july3_1976] +50)))
 july3_2026 <- which.min(abs(data01$time[data01$class=="mod"]- (data01_obs$time[july3_1976] +50))) + nrow(data01_obs)
 july3_2076 <- which.min(abs(data01$time[data01$class=="mod"]- (data01_obs$time[july3_1976] +100))) + nrow(data01_obs)
-
-# compare P2q of the observed 22846 and model 38362 data
-# load("../luna/kristina/MSGpdParam/ukgd_cpm85_5k_x84y20.MSGpdParam.2025-02-26.RData", verbose = TRUE)
-# gpdpar[c(22846,38362),]
-# qgam.p2q.fn[[22845]](data01$u[22845])
-# qgam.p2q.fn[[38362]](data01$u[22846])
-# data01[c(22846,38362),]
-
 
 # load below threshold functions ---------------------------------------------
 folder_p2q_below94 <- "../luna/kristina/P2q/"
@@ -96,6 +88,7 @@ plot(july3_P2q,july3_obs1)
 july3_2026t <- unif_orig_P2q(u=july3_obs,P2q=P2q_sites2,gpdpar = gpdpar_sites2)
 july3_2076t <- unif_orig_P2q(u=july3_obs,P2q=P2q_sites3,gpdpar = gpdpar_sites3)
 tmp <- xyUK20_sf %>% mutate(july3_P2q,july3_2026t,july3_2076t)
+
 # 1. plot illustrating 1976 heatwave in the future ----------------------------
 lims <- c(16,42)
 t1 <- tm_shape(tmp) + tm_dots(fill="july3_P2q",size=0.5,fill.scale = tm_scale_intervals(values="viridis",breaks=c(16,20,24,28,32,36,40,44)),fill.legend = tm_legend(title = "Temperature",reverse = TRUE)) + tm_layout(legend.position=c(0.57,0.95),legend.height = 10,frame=FALSE) + tm_title("July 3, 1976") 
@@ -103,7 +96,6 @@ t2 <- tm_shape(tmp) + tm_dots(fill="july3_2026t",size=0.5,fill.scale = tm_scale_
 t3 <- tm_shape(tmp) + tm_dots(fill="july3_2076t",size=0.5,fill.scale = tm_scale_intervals(values="viridis",breaks=c(16,20,24,28,32,36,40,44)),fill.legend = tm_legend(title = "Temperature",reverse=TRUE)) + tm_layout(legend.show=FALSE,legend.height = 10,frame=FALSE) + tm_title("July 3, 2076 (projection)") 
 t <- tmap_arrange(t1,t2,t3,ncol=3)
 tmap_save(t,filename=paste0(folder_name,"heatwave1976_future.png"),height=6,width=8)
-
 
 # 2. plot illustrating conditioning on a variable -----------------------------
 #Birm_index <- find_site_index(site=Cambridge,grid_uk = xyUK20_sf)
@@ -129,7 +121,7 @@ ggsave(pn,filename=paste0(folder_name,"condmodel_illustrationvL1.png"),width=8,h
 
 # 2. plot of London/Cambridge and London/Lancaster on original margins -------
 tmp <- data_obs_all[,c(Cambridge_index,London_index,Lancaster_index)]
-#names(tmp) <- names(df_sites)[c(1,3,5)]
+names(tmp) <- c("Cambridge","London","Lancaster")
 size_point <- 0.3
 tmp$tf <- tf
 limx <-limy <-  c(7,40)
@@ -142,70 +134,6 @@ p1 <- ggplot(tmp) + geom_point(aes(x=London,y=Cambridge,col=tf),size=size_point)
 p2 <- ggplot(tmp) + geom_point(aes(x=London,y=Lancaster,col=tf),size=size_point) + scale_color_manual(values = c("black", "#009ADA")) + theme(legend.position="none") + coord_fixed() + xlim(limx) + ylim(limy) 
 p <- grid.arrange(p1,p2,ncol=2)
 ggsave(p,filename= paste0(folder_name,"London_Cambridge_Lancaster_original_above.png"),width=8,height=4)
-
-# load to determine index
-load("../luna/kristina/P2q/ukgd_cpm85_5k_x84y20_MSp2q.RData", verbose = TRUE)
-# add column for date
-data01_obs <- data01 %>% rowid_to_column() %>% filter(class=="obs") %>% mutate("date_obs"=seq(ymd('1960-01-01'),ymd('2023-12-31'),by='days') )
-# filter date
-july19_2022 <- data01_obs %>% dplyr::filter(date_obs==lubridate::ymd("2022-07-19")) %>% pull(rowid)
-# find future dates that match from model data
-which.min(abs(data01$time[data01$class=="mod"]- (data01_obs$time[july19_2022] +(2026-2022))))
-july19_2026 <- which.min(abs(data01$time[data01$class=="mod"]- (data01_obs$time[july19_2022] +(2026-2022)))) + nrow(data01_obs)
-date3 <- which.min(abs(data01$time[data01$class=="mod"]- (data01_obs$time[july19_2022] +(2026-2022+50)))) + nrow(data01_obs)
-
-
-# load below threshold functions ---------------------------------------------
-folder_p2q_below94 <- "../luna/kristina/P2q/"
-files <- list.files(folder_p2q_below94)
-P2q_sites1 <-P2q_sites2 <-P2q_sites3 <- list() #create empty list
-# only subset for x in x20 and y in y20
-files_subset <- sapply(1:nrow(xyUK20_sf),function(i){paste0("ukgd_cpm85_5k_x",xyUK20_sf$x[i],"y",xyUK20_sf$y[i],"_MSp2q.RData")})
-#loop through the files
-files_subset1 <- files_subset[files_subset %in% files]
-# could take only x and y divisible by 4 to subset and speed up data loading
-for (i in 1:length(files_subset1)) {
-  print(files_subset1[i])
-  load(paste0(folder_p2q_below94, files_subset1[i]))
-  P2q_sites1[[i]] <- qgam.p2q.fn[[july19_2022]] #add files to list position
-  P2q_sites2[[i]] <- qgam.p2q.fn[[july19_2026]] #add files to list position
-  P2q_sites3[[i]] <- qgam.p2q.fn[[date3]] #add files to list position
-  
-}
-
-# load GPD parameters ---------------------------------------------------------
-folder_gpd_above94 <- "../luna/kristina/MSGpdParam/"
-files <- list.files(folder_gpd_above94)
-gpdpar_sites1 <- gpdpar_sites2 <-gpdpar_sites3 <- as.data.frame(matrix(ncol=3,nrow=nrow(xyUK20_sf))) #create empty dataframe
-names(gpdpar_sites1) <- c("scale","shape","threshold")
-# only subset for x in x20 and y in y20
-files_subset <- sapply(1:nrow(xyUK20_sf),function(i){paste0("ukgd_cpm85_5k_x",xyUK20_sf$x[i],"y",xyUK20_sf$y[i],".MSGpdParam.2025-02-26.RData")})
-#loop through the files
-files_subset1 <- files_subset[files_subset %in% files]
-# could take only x and y divisible by 4 to subset and speed up data loading
-for (i in 1:length(files_subset1)) {
-  print(files_subset1[i])
-  load(paste0(folder_gpd_above94, files_subset1[i]))
-  gpdpar_sites1[i,] <- gpdpar[july19_2022,] #add files to df position
-  gpdpar_sites2[i,] <- gpdpar[july19_2026,] #add files to df position
-  gpdpar_sites3[i,] <- gpdpar[date3,] #add files to df position
-  
-}
-
-# find the hot day in the observed data
-july19_obs <- as.numeric(unlist(data_obs_stationary[(92*(2022-1960)+50),]))
-july19_P2q <- unif_orig_P2q(u=july3_obs,P2q=P2q_sites1,gpdpar = gpdpar_sites1)
-
-# repeat transform for model functions
-july19_2026t <- unif_orig_P2q(u=july19_obs,P2q=P2q_sites2,gpdpar = gpdpar_sites2)
-date3t <- unif_orig_P2q(u=july19_obs,P2q=P2q_sites3,gpdpar = gpdpar_sites3)
-tmp <- xyUK20_sf %>% mutate(july19_P2q,july19_2026t,date3t)
-lims <- c(16,42)
-t1 <- tm_shape(tmp) + tm_dots(fill="july19_P2q",size=0.5,fill.scale = tm_scale_intervals(values="viridis",breaks=c(16,20,24,28,32,36,40,44)),fill.legend = tm_legend(title = "Temperature",reverse = TRUE)) + tm_layout(legend.position=c(0.57,0.95),legend.height = 10,frame=FALSE) + tm_title("July 19, 2022 (TO DO)") 
-t2 <- tm_shape(tmp) + tm_dots(fill="july19_2026t",size=0.5,fill.scale = tm_scale_intervals(values="viridis",breaks=c(16,20,24,28,32,36,40,44)),fill.legend = tm_legend(title = "Temperature",reverse=TRUE)) + tm_layout(legend.position=c(0.57,0.95),legend.height = 10,frame=FALSE) + tm_title("July 19, 2026 (projection)") 
-t3 <- tm_shape(tmp) + tm_dots(fill="date3t",size=0.5,fill.scale = tm_scale_intervals(values="viridis",breaks=c(16,20,24,28,32,36,40,44)),fill.legend = tm_legend(title = "Temperature",reverse=TRUE)) + tm_layout(legend.position=c(0.57,0.95),legend.height = 10,frame=FALSE) + tm_title("July 19, 2076 (projection)") 
-t <- tmap_arrange(t1,t2,t3,ncol=3)
-tmap_save(t,filename=paste0(folder_name,"heatwave2022_future.png"),height=6,width=8)
 
 # simulate 10 fields overall --------------------------------------------------
 # get index for london
@@ -276,17 +204,16 @@ t <- tmap_arrange(t1,t2,t3,t4,t5,t6,nrow=2)
 tmap_save(tm=t, filename=paste0(folder_name,"random3_xcont_sim_London.png"),width=10,height=8)
 
 # 4. AGG density illustration -------------------------------------------------
-Lanc_index <- find_site_index(Lancaster,grid_uk = xyUK20_sf)
 Inverness_index <- find_site_index(Inverness,grid_uk = xyUK20_sf)
 Z1 <- Z %>% add_column(.before=London_index)
 # create a subset of residuals
-tmp <- Z1[,c(Cambridge_index,Lanc_index,Inverness_index)] 
+tmp <- Z1[,c(Cambridge_index,Lancaster_index,Inverness_index)] 
 names(tmp) <- c("Cambridge","Lancaster","Inverness")
 p <- ggplot(tmp%>% pivot_longer(cols=everything())) + geom_histogram(aes(x=value,fill=name)) + labs(fill="Residual site",x="Residual value",y="Count") + scale_fill_manual(values = c("#C11432","#66A64F", "#009ADA")) 
 ggsave(p,filename=paste0(folder_name,"London_residual_site_illustrate.png"),width=6,height=4)
 # change the plot to density
 # get the parameters
-pe3 <- (pe %>% add_row(.before=London_index))[c(Cambridge_index,Lanc_index,Inverness_index),]
+pe3 <- (pe %>% add_row(.before=London_index))[c(Cambridge_index,Lancaster_index,Inverness_index),]
 tr1 <- data.frame(y=as.numeric(),x=as.numeric(),Method=as.character())
 x <- seq(min(Z1),max(Z1),by=0.1)
 for (i in 1:3) {
